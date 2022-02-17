98c00f68aab6629e0d81147c75affddcd9a90f48
Mahiros <95180295+mahiros-tech@users.noreply.github.com>
2022-02-17 03:35:03 +0300

    Remove unnecessary files for our case - 1

--- material/src/config/menuItems.js
+++ material/src/config/menuItems.js
@@ -1,24 +1,8 @@
 import React from 'react'
 import {
-  AccountBox as AccountBoxIcon,
-  ChatBubble,
-  ChromeReaderMode,
   Dashboard as DashboardIcon,
-  ExitToApp as ExitToAppIcon,
-  FilterList,
-  FormatTextdirectionRToL as RTLIcon,
-  FormatTextdirectionLToR as LTRIcon,
   GetApp,
-  InfoOutlined,
-  Language as LanguageIcon,
-  Lock as LockIcon,
-  MenuOpen as MenuOpenIcon,
-  QuestionAnswer,
-  SettingsApplications as SettingsIcon,
-  Style as StyleIcon,
-  Tab,
-  ViewList,
-  Web,
+  Style as StyleIcon
 } from '@mui/icons-material'
 
 //import allLocales from './locales'
@@ -27,22 +11,13 @@ import allThemes from './themes'
 const getMenuItems = (props) => {
   const {
     intl,
-    updateLocale,
-    locale,
-    menuContext,
     themeContext,
     a2HSContext,
-    auth: authData,
   } = props
 
-  const { toggleThis, isDesktop, isAuthMenuOpen, isMiniSwitchVisibility } =
-    menuContext
-  const { themeID, setThemeID, toggleThisTheme } = themeContext
+  const { themeID, setThemeID } = themeContext
 
-  const { auth, setAuth } = authData
   const { isAppInstallable, isAppInstalled, deferredPrompt } = a2HSContext
-
-  const isAuthorised = auth.isAuthenticated
 
   const themeItems = allThemes.map((t) => {
     return {
@@ -55,32 +30,6 @@ const getMenuItems = (props) => {
       leftIcon: <StyleIcon style={{ color: t.color }} />,
     }
   })
-
-  if (isAuthMenuOpen || !isAuthorised) {
-    return [
-      {
-        value: '/my_account',
-        primaryText: intl.formatMessage({
-          id: 'my_account',
-          defaultMessage: 'My Account',
-        }),
-        leftIcon: <AccountBoxIcon />,
-      },
-      {
-        value: '/signin',
-        onClick: isAuthorised
-          ? () => {
-              setAuth({ isAuthenticated: false })
-            }
-          : () => {},
-        visible: true,
-        primaryText: isAuthorised
-          ? intl.formatMessage({ id: 'sign_out' })
-          : intl.formatMessage({ id: 'sign_in' }),
-        leftIcon: isAuthorised ? <ExitToAppIcon /> : <LockIcon />,
-      },
-    ]
-  }
   return [
     {
       value: '/home',
@@ -88,92 +37,13 @@ const getMenuItems = (props) => {
       primaryText: intl.formatMessage({ id: 'home' }),
       leftIcon: <DashboardIcon />,
     },
-    {
-      primaryText: intl.formatMessage({ id: 'demos', defaultMessage: 'Demos' }),
-      primaryTogglesNestedList: true,
-      leftIcon: <Web />,
-      nestedItems: [
-        {
-          value: '/dialog_demo',
-          visible: isAuthorised,
-          primaryText: intl.formatMessage({
-            id: 'dialog_demo',
-            defaultMessage: 'Dialog',
-          }),
-          leftIcon: <ChatBubble />,
-        },
-        {
-          value: '/toast_demo',
-          visible: isAuthorised,
-          primaryText: intl.formatMessage({
-            id: 'toast_demo',
-            defaultMessage: 'Toast',
-          }),
-          leftIcon: <QuestionAnswer />,
-        },
-        {
-          value: '/filter_demo',
-          visible: isAuthorised,
-          primaryText: intl.formatMessage({
-            id: 'filter_demo',
-            defaultMessage: 'Filter',
-          }),
-          leftIcon: <FilterList />,
-        },
-        {
-          value: '/list_page_demo',
-          visible: isAuthorised,
-          primaryText: intl.formatMessage({
-            id: 'list_page_demo_menu',
-            defaultMessage: 'List Page',
-          }),
-          leftIcon: <ViewList />,
-        },
-        {
-          value: '/tabs_demo',
-          visible: isAuthorised,
-          primaryText: intl.formatMessage({
-            id: 'tabs_demo',
-            defaultMessage: 'Tabs Page',
-          }),
-          leftIcon: <Tab />,
-        },
-      ],
-    },
-    {
-      value: '/about',
-      visible: true,
-      primaryText: intl.formatMessage({ id: 'about' }),
-      leftIcon: <InfoOutlined />,
-    },
     { divider: true },
     {
-      primaryText: intl.formatMessage({ id: 'settings' }),
-      primaryTogglesNestedList: true,
-      leftIcon: <SettingsIcon />,
-      nestedItems: [
-        {
-          primaryText: intl.formatMessage({ id: 'theme' }),
-          secondaryText: intl.formatMessage({ id: themeID }),
-          primaryTogglesNestedList: true,
-          leftIcon: <StyleIcon />,
-          nestedItems: themeItems,
-        },
-        {
-          visible: isDesktop ? true : false,
-          onClick: () => {
-            toggleThis('isMiniSwitchVisibility')
-          },
-          primaryText: intl.formatMessage({
-            id: 'menu_mini_mode',
-          }),
-          leftIcon: isMiniSwitchVisibility ? (
-            <MenuOpenIcon />
-          ) : (
-            <ChromeReaderMode />
-          ),
-        },
-      ],
+        primaryText: intl.formatMessage({ id: 'theme' }),
+        secondaryText: intl.formatMessage({ id: themeID }),
+        primaryTogglesNestedList: true,
+        leftIcon: <StyleIcon />,
+        nestedItems: themeItems,
     },
     {
       value: null,
