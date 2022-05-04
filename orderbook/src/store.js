b45dfa8f92b2e3da3383e1a0c90ee3c532615907
Mahiros <mahiros.tech@gmail.com>
2022-05-04 16:40:41 +0300

    Remove old order book data

--- orderbook/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import orderbookReducer from './components/OrderBook/orderbookSlice';
-
-export const store = configureStore({
-  reducer: {
-    orderbook: orderbookReducer,
-  },
-});
