cd811127866d7249f7f437d6d99c03134179e5ba
Mahiros <mahiros.tech@gmail.com>
2022-05-19 10:26:30 +0300

    Remove unnecessary files

--- src/layouts/Logo.js
+++ src/layouts/Logo.js
@@ -21,11 +21,3 @@ export default function Logo({ sx }) {
   return <Box component="img" src={img} sx={{ height: 46, ...sx }} />;
 }
 
-// export default function Logo({ sx }) {
-//   return (
-//     <Fragment>
-//       <Box component="img" src="/static/logo.svg" sx={{ width: 50, height: 50, ...sx }} />
-//       <Box component="img" src="/xrpl/xrp-text-mark-black.svg" sx={{ height: 50, ...sx }} />
-//     </Fragment>
-//   );
-// }
--- src/pages/details/trade/parser/index.js
+++
@@ -1,5 +0,0 @@
-export { default as parseBalanceChanges } from './balancechanges';
-export { default as parseOrderbookChanges } from './orderbookchanges';
-export { default as parseChannelChanges } from './channelchanges';
-export { default as formatOrderBook } from './formatOrderBook';
-export { default as getAffectedAccounts } from './utils';
