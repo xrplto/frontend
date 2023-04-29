26c37298ffcc1aa9ae3d516d08e71af89026389c
cool725 <109250241+cool725@users.noreply.github.com>
2023-04-29 14:03:40 +0200

    updated scrollToTop background

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import {
     alpha,
@@ -8,17 +9,22 @@ import {
     useScrollTrigger,
     Zoom
  } from '@mui/material';
+import { AppContext } from 'src/AppContext';
 
- const FabStyle = styled(Fab)(({ theme }) => ({
+ const FabStyle = styled(Fab)(({ theme, darkMode }) => ({
     boxShadow: 'none',
     backdropFilter: 'blur(2px)',
     WebkitBackdropFilter: 'blur(2px)', // Fix on Mobile
     //backgroundColor: alpha(theme.palette.background.default, 0.9),
     //color: alpha("#00AB88", 0.7),
     //backgroundColor: alpha("#00AB88", 0.7),
-    backgroundColor: alpha("#9E86FF", 0.7),
+    // backgroundColor: alpha("#9E86FF", 0.7),
+    // '&:hover': {
+    //     backgroundColor: alpha("#9E86FF", 0.4),
+    // },
+    backgroundColor: darkMode ? 'rgb(114, 114, 114)' : 'rgba(23, 24, 27, 0.85)',
     '&:hover': {
-        backgroundColor: alpha("#9E86FF", 0.4),
+        backgroundColor: 'rgb(41, 41, 41)',
     },
 }));
 
@@ -40,6 +46,8 @@ export default function ScrollToTop(props) {
         disableHysteresis: true,
         threshold: 100,
     });
+
+    const { darkMode } = useContext(AppContext);
   
     const handleClick = (event) => {
       const anchor = (event.target.ownerDocument || document).querySelector(
@@ -61,7 +69,7 @@ export default function ScrollToTop(props) {
                 role="presentation"
                 sx={{ position: 'fixed', bottom: 16, right: 16 }}
             >
-                <FabStyle size="small" aria-label="scroll back to top">
+                <FabStyle size="small" aria-label="scroll back to top" darkMode={darkMode}>
                     <KeyboardArrowUpIcon />
                 </FabStyle>
             </Box>
