e05383798d0a823649b63b59ade8565fb47ebbd9
XRPL.to <93736363+xrplto@users.noreply.github.com>
2023-05-28 14:52:25 -0400

    Update LoginDialog.js
    Updated Styling and UI of Xaman QR Wallet Connect.

--- src/components/LoginDialog.js
+++ src/components/LoginDialog.js
@@ -1,82 +1,90 @@
-import {
-  alpha,
-  styled,
-  Box,
-  Dialog as MuiDialog,
-  Link,
-  Typography
-  // DialogTitle,
-  //Divider
-} from '@mui/material';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Link,
+  Typography,
+  CircularProgress,
+  styled
+} from '@mui/material';
 import { isMobile } from 'react-device-detect';
-import Dialog from './Dialog';
-import { useState } from 'react';
 
-const QRDialog = styled(MuiDialog)(({ theme }) => ({
-  //boxShadow: theme.customShadows.z0,
-  // backdropFilter: 'blur(2px)',
-  // WebkitBackdropFilter: 'blur(2px)', // Fix on Mobile
-  // backgroundColor: alpha(theme.palette.background.paper, 0.0),
-  // borderRadius: '0px',
-  // padding: '0.5em'
-  // backgroundColor: alpha("#00AB88", 0.99),
+const QRDialog = styled(Dialog)(({ theme }) => ({
   zIndex: 1302
 }));
 
-const LinkTypography = styled(Typography)(({ theme }) => ({
-  // backgroundColor: alpha(theme.palette.background.paper, 0.0),
+const StyledDialogContent = styled(DialogContent)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: theme.spacing(2)
+}));
+
+const StyledQRImage = styled('img')({
+  maxWidth: '100%',
+  height: 'auto'
+});
+
+const StyledLinkTypography = styled(Typography)(({ theme }) => ({
   borderRadius: '2px',
-  border: '0px solid #00AB88',
-  padding: '0.5em'
-  // backgroundColor: alpha("#00AB88", 0.99),
+  border: `1px solid ${theme.palette.primary.main}`,
+  padding: theme.spacing(1),
+  marginTop: theme.spacing(2),
+  cursor: 'pointer',
+  transition: 'background-color 0.2s ease-in-out',
+  '&:hover': {
+    backgroundColor: theme.palette.primary.light
+  }
 }));
 
 export default function LoginDialog(props) {
-  const qrUrl = props.qrUrl;
-  const nextUrl = props.nextUrl;
-
+  const { qrUrl, nextUrl, open, handleClose } = props;
   const [openDialog, setOpenDialog] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const history = useRouter();
+  const router = useRouter();
 
-  const onClose = () => {
-    props.handleClose();
-  };
-
-  const handleClick = (e) => {
+  const handleClick = async (e) => {
     e.preventDefault();
 
     if (!isMobile) {
-      history.push(nextUrl);
+      router.push(nextUrl);
     } else {
       setOpenDialog(true);
     }
   };
 
-  const handleClose = () => {
+  const handleCloseDialog = () => {
     setOpenDialog(false);
   };
 
   const handleRedirect = () => {
-    history.push(nextUrl);
+    setLoading(true);
+
+    // Simulating an API request delay
+    setTimeout(() => {
+      setLoading(false);
+      router.push(nextUrl);
+    }, 1500);
   };
 
   return (
-    <QRDialog onClose={onClose} open={props.open}>
-      {/* <DialogTitle>Scan the QR code from your XUMM app</DialogTitle> */}
-      {/* <Divider /> */}
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Box component="img" sx={{}} alt="QR" src={qrUrl} />
-
+    <QRDialog onClose={handleClose} open={open}>
+      <DialogTitle>
+      <Typography variant="h3" align="center" gutterBottom>
+          Connect Wallet
+        </Typography>
+        Scan the QR code from your Xaman app
+        </DialogTitle>
+      <StyledDialogContent>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <StyledQRImage alt="QR" src={qrUrl} />
+        </Box>
         <Link
           underline="none"
           color="inherit"
@@ -85,17 +93,40 @@ export default function LoginDialog(props) {
           rel="noreferrer noopener nofollow"
           onClick={handleClick}
         >
-          <LinkTypography variant="h4" color="primary">
-            Open in XUMM
-          </LinkTypography>
+          <StyledLinkTypography variant="h4" color="primary">
+            Open in Xaman
+          </StyledLinkTypography>
         </Link>
-      </div>
-
+        {loading && (
+          <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 8 }}>
+            <CircularProgress />
+          </Box>
+        )}
+        {error && (
+          <Typography variant="body2" color="error" align="center" sx={{ marginTop: 2 }}>
+            {error}
+          </Typography>
+        )}
+      </StyledDialogContent>
       <Dialog
         open={openDialog}
-        handleClose={handleClose}
-        handleRedirect={handleRedirect}
-      />
+        onClose={handleCloseDialog}
+        onBackdropClick={handleCloseDialog}
+        disableEscapeKeyDown={loading}
+      >
+        <StyledDialogContent>
+          <Typography variant="body1" align="center" gutterBottom>
+            Confirm your action in the Xaman app.
+          </Typography>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <StyledLinkTypography variant="h6" color="primary" onClick={handleRedirect}>
+              Continue in Xaman
+            </StyledLinkTypography>
+          )}
+        </StyledDialogContent>
+      </Dialog>
     </QRDialog>
   );
 }
