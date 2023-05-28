b6dae5ee148e10d4d0f96d1194f3d2062fa27294
Yannier <93131737+yannierlesende@users.noreply.github.com>
2023-05-28 19:12:34 -0400

    Update QRDialog.js
    Updated styling of QR notification.

--- src/components/QRDialog.js
+++ src/components/QRDialog.js
@@ -1,137 +1,128 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { alpha, styled, useTheme, useMediaQuery, Box, Dialog, DialogContent, DialogTitle, IconButton, Link, Stack, Typography } from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
 
-// Material
-import {
-    alpha, styled, useTheme, useMediaQuery,
-    Box,
-    Dialog,
-    Divider,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
-    IconButton,
-    Link,
-    Stack,
-    Typography
-} from '@mui/material';
-
-import {
-    Close as CloseIcon
-} from '@mui/icons-material';
-
-const ExDialog = styled(Dialog) (({ theme }) => ({
-    backdropFilter: 'blur(2px)',
-    WebkitBackdropFilter: 'blur(2px)', // Fix on Mobile
-    backgroundColor: alpha(theme.palette.background.paper, 0.0),
-    borderRadius: '0px',
-    '& .MuiDialogContent-root': {
-        padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-        padding: theme.spacing(1),
-    },
+const CustomDialog = styled(Dialog)(({ theme }) => ({
+  backdropFilter: 'blur(2px)',
+  WebkitBackdropFilter: 'blur(2px)', // Fix on Mobile
+  backgroundColor: alpha(theme.palette.background.paper, 0.0),
+  borderRadius: '0px',
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
 }));
 
-const LinkTypography = styled(Typography)(({ theme }) => ({
-    // backgroundColor: alpha(theme.palette.background.paper, 0.0),
+const StyledLinkTypography = styled(Typography)(({ theme }) => ({
     borderRadius: '2px',
-    border: '0px solid #00AB88',
-    padding: '0.5em',
-    // backgroundColor: alpha("#00AB88", 0.99),
-}));
-  
-const ExDialogTitle = (props) => {
-    const { children, onClose, ...other } = props;
+    border: `1px solid ${theme.palette.primary.main}`,
+    padding: theme.spacing(1),
+    marginTop: theme.spacing(2),
+    cursor: 'pointer',
+    transition: 'background-color 0.2s ease-in-out',
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light
+    }
+  }));
 
-    return (
-        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-            {children}
-            {onClose ? (
-                <IconButton
-                    aria-label="close"
-                    onClick={onClose}
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            ) : null}
-        </DialogTitle>
-    );
+const CustomDialogTitle = (props) => {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
 };
 
-export default function QRDialog({open, type, qrUrl, nextUrl, onClose}) {
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+export default function CustomQRDialog({ open, type, qrUrl, nextUrl, onClose }) {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
-    const [showQR, setShowQR] = useState(false);
+  const [showQR, setShowQR] = useState(false);
+  const [notificationClicked, setNotificationClicked] = useState(false);
 
-    useEffect(() => {
-        setShowQR(false);
-    }, [open]);
+  useEffect(() => {
+    if (open) {
+      setShowQR(false);
+      setNotificationClicked(false);
+    }
+  }, [open]);
 
-    return (
-        <ExDialog
-            fullScreen={fullScreen}
-            fullWidth={true}
-            maxWidth='xs'
-            onClose={onClose}
-            open={open}
-            sx={{ zIndex: 1303 }}
+  const handleQRButtonClick = () => {
+    setShowQR(true);
+    setNotificationClicked(true);
+  };
+
+  return (
+    <CustomDialog
+      fullScreen={fullScreen}
+      fullWidth={true}
+      maxWidth="xs"
+      onClose={onClose}
+      open={open}
+      sx={{ zIndex: 1303 }}
+    >
+      
+      <DialogContent dividers>
+        <Stack alignItems="center" spacing={2}>
+          <Typography variant="h3">{type}</Typography>
+          <Typography variant="subtitle1">Sign the transaction on your Xaman App</Typography>
+          {!notificationClicked && (
+            <Link
+              component="button"
+              underline="hover"
+              variant="body2"
+              color="inherit"
+              onClick={handleQRButtonClick}
+            >
+              <Typography variant="caption" color="error">
+                Didn't receive a notification? Click here to scan QR!
+              </Typography>
+            </Link>
+          )}
+        </Stack>
+        <div
+          style={{
+            display: showQR ? 'flex' : 'none',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingBottom: 50,
+            marginTop: 50,
+          }}
         >
-            <ExDialogTitle textAlign="center" onClose={onClose}>
-                Sign Transaction
-            </ExDialogTitle>
+          <Box component="img" alt="QR" src={qrUrl} sx={{ mb: 2 }} />
 
-            <DialogContent dividers>
-                <Stack alignItems='center' spacing={2}>
-                    <Typography variant='subtitle1'>{type}</Typography>
-                    <Typography variant='subtitle1'>Sign the transaction on your XUMM App</Typography>
-                    <Link
-                        component="button"
-                        underline="hover"
-                        variant="body2"
-                        color="inherit"
-                        onClick={() => {
-                            setShowQR(true);
-                        }}
-                    >
-                        <Typography variant='caption' color='error'>Didn't receive a notification? Click here to scan QR!</Typography>
-                    </Link>
-                </Stack>
-                <div
-                    style={{
-                        display: showQR?"flex":"none",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        paddingBottom: 50,
-                        marginTop: 50
-                    }}
-                >
-
-                    <Box
-                        component="img"
-                        alt="QR"
-                        src={qrUrl}
-                        sx={{mb:2}}
-                    />
-                    
-                    <Link
-                        underline="none"
-                        color="inherit"
-                        target="_blank"
-                        href={nextUrl}
-                        rel="noreferrer noopener nofollow"
-                    >
-                        <LinkTypography variant="subtitle2" color='primary'>Open in XUMM</LinkTypography>
-                    </Link>
-                </div>
-            </DialogContent>
-        </ExDialog>
-    );
+          <Link
+            underline="none"
+            color="inherit"
+            target="_blank"
+            href={nextUrl}
+            rel="noreferrer noopener nofollow"
+          >
+            <StyledLinkTypography variant="h4" color="primary">
+              Open in Xaman
+            </StyledLinkTypography>
+          </Link>
+        </div>
+      </DialogContent>
+    </CustomDialog>
+  );
 }
