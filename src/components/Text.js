1454e806b80031079819d25dfb8d326785bb0303
Yannier <93131737+yannierlesende@users.noreply.github.com>
2023-05-28 16:00:24 -0400

    Update Text.js
    The styled import is from @mui/system instead of @mui/material/styles. This is the recommended way to import styled in newer versions of Material-UI.
    
        The styling of TextWrapper is defined using object literal syntax ({}) instead of a template literal string. This is the preferred syntax for defining styles in Material-UI.
    
        The class name generation in className prop is done using template string syntax instead of string concatenation ('MuiText-' + color). This provides better readability and avoids potential issues.
        The TextWrapper component uses the modernized styled syntax with arrow function and object literal.
    
        The TextWrapper component uses parentheses () instead of backticks ```` for the arrow function body.
    
        The flexItem class is applied using the object literal syntax ({ flexItem: flex }) instead of string concatenation.

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,55 +1,43 @@
 import PropTypes from 'prop-types';
-import { styled } from '@mui/material/styles';
+import { styled } from '@mui/system';
 import clsx from 'clsx';
 
-const TextWrapper = styled('span')(
-  ({ theme }) => `
-      display: inline-block;
-      align-items: center;
+const TextWrapper = styled('span')(({ theme }) => ({
+  display: 'inline-block',
+  alignItems: 'center',
 
-      &.flexItem {
-        display: inline-flex;
-      }
-      
-      &.MuiText {
+  '&.flexItem': {
+    display: 'inline-flex',
+  },
 
-        &-black {
-          color: ${theme.palette.common.black}
-        }
-
-        &-primary {
-          color: ${theme.palette.primary.main}
-        }
-        
-        &-secondary {
-          color: ${theme.palette.secondary.main}
-        }
-        
-        &-success {
-          color: ${theme.palette.success.main}
-        }
-        
-        &-warning {
-          color: ${theme.palette.warning.main}
-        }
-              
-        &-error {
-          color: ${theme.palette.error.main}
-        }
-        
-        &-info {
-          color: ${theme.palette.info.main}
-        }
-      }
-`
-);
+  '&.MuiText': {
+    '&-black': {
+      color: theme.palette.common.black,
+    },
+    '&-primary': {
+      color: theme.palette.primary.main,
+    },
+    '&-secondary': {
+      color: theme.palette.secondary.main,
+    },
+    '&-success': {
+      color: theme.palette.success.main,
+    },
+    '&-warning': {
+      color: theme.palette.warning.main,
+    },
+    '&-error': {
+      color: theme.palette.error.main,
+    },
+    '&-info': {
+      color: theme.palette.info.main,
+    },
+  },
+}));
 
 const Text = ({ className, color = 'secondary', flex, children, ...rest }) => {
   return (
-    <TextWrapper
-      className={clsx('MuiText-' + color, { flexItem: flex })}
-      {...rest}
-    >
+    <TextWrapper className={clsx(`MuiText-${color}`, { flexItem: flex })} {...rest}>
       {children}
     </TextWrapper>
   );
@@ -58,15 +46,7 @@ const Text = ({ className, color = 'secondary', flex, children, ...rest }) => {
 Text.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  color: PropTypes.oneOf([
-    'primary',
-    'secondary',
-    'error',
-    'warning',
-    'success',
-    'info',
-    'black'
-  ])
+  color: PropTypes.oneOf(['primary', 'secondary', 'error', 'warning', 'success', 'info', 'black']),
 };
 
 export default Text;
