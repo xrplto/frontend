d815fbb01014be42f7b1d34ec2f4d17bb00677f7
XRPL.to <93736363+xrplto@users.noreply.github.com>
2023-05-29 19:31:52 -0400

    Update Dialog.js

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,32 +1,30 @@
-import * as React from 'react';
+import React, { forwardRef } from 'react';
 import Button from '@mui/material/Button';
-import { Dialog as MuiDialog } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 
-const Transition = React.forwardRef(function Transition(props, ref) {
+const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function Dialog({ open, handleRedirect, handleClose }) {
+export default function CustomDialog({ open, handleRedirect, handleClose }) {
   return (
-    <div>
-      <MuiDialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-        maxWidth={'xl'}
-        fullWidth={true}
-      >
-        <DialogTitle>{'Open in XUMM?'}</DialogTitle>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleRedirect}>Open</Button>
-        </DialogActions>
-      </MuiDialog>
-    </div>
+    <Dialog
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+      maxWidth="xl"
+      fullWidth
+    >
+      <DialogTitle>Open in XUMM?</DialogTitle>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleRedirect}>Open</Button>
+      </DialogActions>
+    </Dialog>
   );
 }
