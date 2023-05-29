b21c450e8117bd1a379674b0b269c3d3d53cbf25
Yannier <93131737+yannierlesende@users.noreply.github.com>
2023-05-28 20:54:08 -0400

    Update Logo.js
    Organize imports: Group your imports based on their source to improve readability and maintainability. It's a good practice to separate third-party libraries from local imports.
    
    Destructure AppContext: Instead of accessing darkMode as const { darkMode } = useContext(AppContext);, you can destructure it directly in the function's parameters.
    
    Use template literals for image paths: You can use template literals to construct the image paths dynamically based on the darkMode value. This can help improve code readability and reduce duplication.
    
    Simplify link styles: Instead of using the sx prop to apply styles to the link component, you can define the styles directly in the component declaration to keep the JSX code cleaner.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,35 +1,26 @@
 import { useContext } from 'react';
-import { AppContext } from 'src/AppContext';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import {
-    Box,
-    Link
-} from '@mui/material';
+import { Box, Link } from '@mui/material';
+
+import { AppContext } from 'src/AppContext';
 
 function Logo({ style }) {
-    const { darkMode } = useContext(AppContext);
+  const { darkMode } = useContext(AppContext);
 
-    const img_black = "/logo/xrpl-to-logo-black.svg";
-    const img_white = "/logo/xrpl-to-logo-white.svg";
-    
-    const img = darkMode?img_white:img_black;
-    
-    return (
-        <Link
-            href="/"
-            sx={{ pl: 0, pr: 0, py: 3, display: 'inline-flex' }}
-            underline="none"
-            rel="noreferrer noopener nofollow"
-            style={style}
-        >
-            {/* <Box component="img" src={img} sx={{ height: 46 }} /> */}
-            <LazyLoadImage
-                src={img}
-                width={125}
-                height={46}
-            />
-        </Link>
-    );
+  const img_black = "/logo/xrpl-to-logo-black.svg";
+  const img_white = "/logo/xrpl-to-logo-white.svg";
+  const img = darkMode ? img_white : img_black;
+
+  return (
+    <Link
+      href="/"
+      style={{ paddingLeft: 0, paddingRight: 0, paddingTop: 3, display: 'inline-flex', ...style }}
+      underline="none"
+      rel="noreferrer noopener nofollow"
+    >
+      <LazyLoadImage src={img} width={125} height={46} />
+    </Link>
+  );
 }
 
 export default Logo;
