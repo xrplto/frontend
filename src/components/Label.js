a1ce6f148094fd5ed35a76c1947e4c401ca66cb9
Yannier <93131737+yannierlesende@users.noreply.github.com>
2023-05-28 21:51:09 -0400

    Update Label.js
    Import specific components from 'prop-types':
    Instead of importing the entire 'prop-types' module, you can import only the necessary components to reduce the bundle size and improve performance
    
    Use object destructuring for 'theme' variable:
    Instead of accessing the 'theme' object directly from the props argument, you can use object destructuring to extract it directly. Update the relevant line in the styled 
    
    Avoid string concatenation in className assignment:
    Instead of manually concatenating the className value, you can use template literals for a more concise and readable approach. 
    
    Use more meaningful prop names:
    Consider using more descriptive prop names instead of generic ones like 'color' and 'className'. For example, you could rename 'color' to 'variant' to better indicate the purpose of the prop.

--- src/components/Label.js
+++ src/components/Label.js
@@ -1,76 +1,67 @@
 import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
 
-const LabelWrapper = styled('span')(
-  ({ theme }) => `
-      background-color: ${theme.colors.alpha.black[5]};
-      padding: ${theme.spacing(0.5, 1)};
-      font-size: ${theme.typography.pxToRem(13)};
-      border-radius: ${theme.general.borderRadius};
-      display: inline-flex;
-      align-items: center;
-      justify-content: center;
-      max-height: ${theme.spacing(3)};
-      
-      &.MuiLabel {
-        &-primary {
-          background-color: ${theme.colors.primary.lighter};
-          color: ${theme.palette.primary.main}
-        }
+const { node, string, oneOf } = PropTypes;
 
-        &-black {
-          background-color: ${theme.colors.alpha.black[100]};
-          color: ${theme.colors.alpha.white[100]};
-        }
-        
-        &-secondary {
-          background-color: ${theme.colors.secondary.lighter};
-          color: ${theme.palette.secondary.main}
-        }
-        
-        &-success {
-          background-color: ${theme.colors.success.lighter};
-          color: ${theme.palette.success.main}
-        }
-        
-        &-warning {
-          background-color: ${theme.colors.warning.lighter};
-          color: ${theme.palette.warning.main}
-        }
-              
-        &-error {
-          background-color: ${theme.colors.error.lighter};
-          color: ${theme.palette.error.main}
-        }
-        
-        &-info {
-          background-color: ${theme.colors.info.lighter};
-          color: ${theme.palette.info.main}
-        }
-      }
-`
+const LabelWrapper = styled('span')(({ theme }) => ({
+  backgroundColor: theme.colors.alpha.black[5],
+  padding: theme.spacing(0.5, 1),
+  fontSize: theme.typography.pxToRem(13),
+  borderRadius: theme.general.borderRadius,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  maxHeight: theme.spacing(3),
+
+  '&.MuiLabel': {
+    '&-primary': {
+      backgroundColor: theme.colors.primary.lighter,
+      color: theme.palette.primary.main,
+    },
+    '&-black': {
+      backgroundColor: theme.colors.alpha.black[100],
+      color: theme.colors.alpha.white[100],
+    },
+    '&-secondary': {
+      backgroundColor: theme.colors.secondary.lighter,
+      color: theme.palette.secondary.main,
+    },
+    '&-success': {
+      backgroundColor: theme.colors.success.lighter,
+      color: theme.palette.success.main,
+    },
+    '&-warning': {
+      backgroundColor: theme.colors.warning.lighter,
+      color: theme.palette.warning.main,
+    },
+    '&-error': {
+      backgroundColor: theme.colors.error.lighter,
+      color: theme.palette.error.main,
+    },
+    '&-info': {
+      backgroundColor: theme.colors.info.lighter,
+      color: theme.palette.info.main,
+    },
+  },
+}));
+
+const Label = ({ variant = 'secondary', children, ...rest }) => (
+  <LabelWrapper className={`MuiLabel-${variant}`} {...rest}>
+    {children}
+  </LabelWrapper>
 );
 
-const Label = ({ className, color = 'secondary', children, ...rest }) => {
-  return (
-    <LabelWrapper className={'MuiLabel-' + color} {...rest}>
-      {children}
-    </LabelWrapper>
-  );
-};
-
 Label.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  color: PropTypes.oneOf([
+  children: node,
+  variant: oneOf([
     'primary',
     'black',
     'secondary',
     'error',
     'warning',
     'success',
-    'info'
-  ])
+    'info',
+  ]),
 };
 
 export default Label;
