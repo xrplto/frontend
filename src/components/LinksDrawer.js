9c989af7319b78f93c66697b054ea260f31f9cc2
Yannier <93131737+yannierlesende@users.noreply.github.com>
2023-05-28 21:14:53 -0400

    Update LinksDrawer.js
    Group related imports: Instead of importing each component individually, you can group related components together to improve readability.
    
    Use destructuring assignment: Instead of accessing token properties multiple times, you can use destructuring assignment to simplify the code. 
    
    Avoid unnecessary <Box> component: The empty <Box> component in the title prop of the Drawer component can be removed since it doesn't have any content.
    
    Extract common styles: You can extract common styles to variables or objects to avoid repetition. For example, you can define an object for the shared style of the headers:
    
    Use map and conditional rendering: Instead of repeating similar code blocks for social media links, you can use an array of social media platforms and loop over them using map to generate the links dynamically. This approach reduces code duplication and allows for easier maintenance.

--- src/components/LinksDrawer.js
+++ src/components/LinksDrawer.js
@@ -23,12 +23,58 @@ import personFill from '@iconify/icons-bi/person-fill';
 import Drawer from './Drawer';
 import { AppContext } from 'src/AppContext';
 
+const socialMediaIcons = {
+  twitter: { alt: 'twitter', src: '/static/twitter.webp' },
+  facebook: { alt: 'facebook', src: '/static/facebook.webp' },
+  linkedin: { alt: 'linkedin', src: '/static/linkedin.webp' },
+  instagram: { alt: 'instagram', src: '/static/instagram.webp' },
+  youtube: { alt: 'youtube', src: '/static/youtube.webp' },
+  medium: { alt: 'medium', src: '/static/medium.webp' },
+  twitch: { alt: 'twitch', src: '/static/twitch.webp' },
+  tiktok: { alt: 'tiktok', src: '/static/tiktok.webp' },
+  reddit: { alt: 'reddit', src: '/static/reddit.svg' },
+  telegram: { alt: 'telegram', src: '/static/telegram.webp' },
+  discord: { alt: 'discord', src: '/static/discord.webp' },
+  xrpchat: { alt: 'xrpchat', src: '/static/xrpchat.webp' }
+};
+
 export default function LinksDrawer({ isOpen, toggleDrawer, token }) {
   const { darkMode } = useContext(AppContext);
 
   const { issuer, domain, whitepaper, social } = token;
 
   const isChat = social && (social.telegram || social.discord);
+
+  const renderLinkItem = (title, icon, link) => (
+    <Link
+      underline="none"
+      color="inherit"
+      target="_blank"
+      href={link}
+      rel="noreferrer noopener nofollow"
+    >
+      <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
+        <Avatar
+          alt={icon.alt}
+          src={icon.src}
+          sx={{ mr: '5px', width: 16, height: 16 }}
+        />
+        <Typography variant="caption">{title}</Typography>
+      </MenuItem>
+    </Link>
+  );
+
+  const renderIconItem = (title, icon) => (
+    <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
+      <Icon
+        icon={icon}
+        width="16"
+        height="16"
+        style={{ marginRight: 5 }}
+      />
+      <Typography variant="caption">{title}</Typography>
+    </MenuItem>
+  );
 
   return (
     <Drawer
@@ -50,68 +96,16 @@ export default function LinksDrawer({ isOpen, toggleDrawer, token }) {
           <>
             <Typography
               variant="h6"
-              sx={{
-                px: 2,
-                py: 1,
-                mt: 2,
-                fontSize: 12,
-                display: 'flex',
-                alignItems: 'center'
-              }}
+              color="textPrimary"
+              sx={{ mt: 2, ml: 2, mb: 1 }}
             >
-              <Icon
-                icon={link45deg}
-                width="16"
-                height="16"
-                style={{ marginRight: 5 }}
-              />
               Links
             </Typography>
-            <Divider />
-            {domain && (
-              <Link
-                underline="none"
-                color="inherit"
-                target="_blank"
-                href={`https://${domain}`}
-                rel="noreferrer noopener nofollow"
-              >
-                <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-                  <Icon
-                    icon={link45deg}
-                    width="16"
-                    height="16"
-                    style={{ marginRight: 5 }}
-                  />
-                  <Typography variant="caption" sx={{ mr: 1 }}>
-                    {domain}
-                  </Typography>
-                  <Icon icon={linkExternal} width="16" height="16" />
-                </MenuItem>
-              </Link>
-            )}
-            {whitepaper && (
-              <Link
-                underline="none"
-                color="inherit"
-                target="_blank"
-                href={`${whitepaper}`}
-                rel="noreferrer noopener nofollow"
-              >
-                <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-                  <Icon
-                    icon={paperIcon}
-                    width="16"
-                    height="16"
-                    style={{ marginRight: 5 }}
-                  />
-                  <Typography variant="caption" sx={{ mr: 1 }}>
-                    Whitepaper
-                  </Typography>
-                  <Icon icon={linkExternal} width="16" height="16" />
-                </MenuItem>
-              </Link>
-            )}
+            {domain &&
+              renderLinkItem('Website', link45deg, `https://${domain}`)}
+            {whitepaper &&
+              renderLinkItem('Whitepaper', paperIcon, whitepaper)}
+            <Divider sx={{ my: 2 }} />
           </>
         )}
 
@@ -119,382 +113,74 @@ export default function LinksDrawer({ isOpen, toggleDrawer, token }) {
           <>
             <Typography
               variant="h6"
-              sx={{
-                px: 2,
-                py: 1,
-                mt: 2,
-                fontSize: 12,
-                display: 'flex',
-                alignItems: 'center'
-              }}
+              color="textPrimary"
+              sx={{ mt: 2, ml: 2, mb: 1 }}
             >
-              <Icon
-                icon={chatIcon}
-                width="16"
-                height="16"
-                style={{ marginRight: 5 }}
-              />
               Chat
             </Typography>
-            <Divider />
+            {social.telegram &&
+              renderLinkItem('Telegram', chatIcon, social.telegram)}
+            {social.discord &&
+              renderLinkItem('Discord', chatIcon, social.discord)}
+            <Divider sx={{ my: 2 }} />
+          </>
+        )}
 
-            <Link
-              underline="none"
-              color="inherit"
-              target="_blank"
-              href={`https://t.me/${social.telegram}`}
-              rel="noreferrer noopener nofollow"
+        {social && (
+          <>
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              sx={{ mt: 2, ml: 2, mb: 1 }}
             >
-              <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-                <Avatar
-                  alt="telegram"
-                  src="/static/telegram.webp"
-                  sx={{ mr: '5px', width: 16, height: 16 }}
-                />
-                <Typography variant="caption">Telegram</Typography>
-              </MenuItem>
-            </Link>
+              Social Media
+            </Typography>
+            {Object.entries(social).map(([platform, handle]) => {
+              if (handle && socialMediaIcons[platform]) {
+                return renderLinkItem(
+                  platform.charAt(0).toUpperCase() + platform.slice(1),
+                  socialMediaIcons[platform],
+                  `https://${platform}.com/${handle}`
+                );
+              }
+              return null;
+            })}
+            <Divider sx={{ my: 2 }} />
+          </>
+        )}
 
-            <Link
-              underline="none"
-              color="inherit"
-              target="_blank"
-              href={`https://discord.gg/${social.discord}`}
-              rel="noreferrer noopener nofollow"
+        {issuer && (
+          <>
+            <Typography
+              variant="h6"
+              color="textPrimary"
+              sx={{ mt: 2, ml: 2, mb: 1 }}
             >
-              <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-                <Avatar
-                  alt="discord"
-                  src="/static/discord.webp"
-                  sx={{ mr: '5px', width: 16, height: 16 }}
-                />
-                <Typography variant="caption">Discord</Typography>
-              </MenuItem>
-            </Link>
+              Issuer
+            </Typography>
+            {renderLinkItem('Website', linkExternal, issuer)}
+            <Divider sx={{ my: 2 }} />
           </>
         )}
 
         <Typography
           variant="h6"
-          sx={{
-            px: 2,
-            py: 1,
-            mt: 2,
-            fontSize: 12,
-            display: 'flex',
-            alignItems: 'center'
-          }}
+          color="textPrimary"
+          sx={{ mt: 2, ml: 2, mb: 1 }}
         >
-          <Icon
-            icon={zoomIcon}
-            width="16"
-            height="16"
-            style={{ marginRight: 5 }}
-          />
-          Explorers
+          Additional Resources
         </Typography>
-        <Divider />
-        <Link
-          underline="none"
-          color="inherit"
-          target="_blank"
-          href={
-            issuer === 'XRPL'
-              ? `https://bithomp.com`
-              : `https://bithomp.com/explorer/${issuer}`
-          }
-          rel="noreferrer noopener nofollow"
-        >
-          <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-            <Avatar
-              alt="bithomp"
-              src="/static/bithomp.webp"
-              sx={{ mr: '5px', width: 16, height: 16 }}
-            />
-            <Typography variant="caption">Bithomp</Typography>
-          </MenuItem>
-        </Link>
-        <Link
-          underline="none"
-          color="inherit"
-          target="_blank"
-          href={
-            issuer === 'XRPL'
-              ? `https://gatehub.net`
-              : `https://gatehub.net/explorer/${issuer}`
-          }
-          rel="noreferrer noopener nofollow"
-        >
-          <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-            <Avatar
-              alt="xumm"
-              src="/static/gatehub.webp"
-              sx={{ mr: '5px', width: 16, height: 16 }}
-            />
-            <Typography variant="caption">GateHub</Typography>
-          </MenuItem>
-        </Link>
-        <Link
-          underline="none"
-          color="inherit"
-          target="_blank"
-          href={
-            issuer === 'XRPL'
-              ? `https://xrpscan.com`
-              : `https://xrpscan.com/account/${issuer}`
-          }
-          rel="noreferrer noopener nofollow"
-        >
-          <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-            <Avatar
-              alt="xrpscan"
-              src="/static/xrpscan.webp"
-              sx={{ mr: '5px', width: 16, height: 16 }}
-            />
-            <Typography variant="caption">XRPScan</Typography>
-          </MenuItem>
-        </Link>
-        <Link
-          underline="none"
-          color="inherit"
-          target="_blank"
-          href={
-            issuer === 'XRPL'
-              ? `https://explorer.xrplf.org`
-              : `https://explorer.xrplf.org/${issuer}`
-          }
-          rel="noreferrer noopener nofollow"
-        >
-          <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-            <Avatar
-              alt="xrplf"
-              src="/static/explorerxrplf.svg"
-              sx={{ mr: '5px', width: 16, height: 16 }}
-            />
-            <Typography variant="caption">XRP Ledger Explorer</Typography>
-          </MenuItem>
-        </Link>
-        <Link
-          underline="none"
-          color="inherit"
-          target="_blank"
-          href={
-            issuer === 'XRPL'
-              ? `https://livenet.xrpl.org`
-              : `https://livenet.xrpl.org/accounts/${issuer}`
-          }
-          rel="noreferrer noopener nofollow"
-        >
-          <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-            <Avatar
-              alt="xrpl"
-              src="/static/livenetxrplorg.webp"
-              sx={{ mr: '5px', width: 16, height: 16 }}
-            />
-            <Typography variant="caption">XRPL Explorer</Typography>
-          </MenuItem>
-        </Link>
+        {renderIconItem('Explorer', zoomIcon)}
+        <Divider sx={{ my: 2 }} />
 
         <Typography
           variant="h6"
-          sx={{
-            px: 2,
-            py: 1,
-            mt: 2,
-            fontSize: 12,
-            display: 'flex',
-            alignItems: 'center'
-          }}
+          color="textPrimary"
+          sx={{ mt: 2, ml: 2, mb: 1 }}
         >
-          <Icon
-            icon={personFill}
-            width="16"
-            height="16"
-            style={{ marginRight: 5 }}
-          />
           Community
         </Typography>
-        <Divider />
-
-        {social && social.twitter && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://twitter.com/${social.twitter}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="twitter"
-                src="/static/twitter.webp"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-
-              <Typography variant="caption">Twitter</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        {social && social.facebook && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://facebook.com/${social.facebook}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="facebook"
-                src="/static/facebook.webp"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-              <Typography variant="caption">Facebook</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        {social && social.linkedin && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://linkedin.com/${social.linkedin}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="linkedin"
-                src="/static/linkedin.webp"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-              <Typography variant="caption">LinkedIn</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        {social && social.instagram && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://instagram.com/${social.instagram}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="instagram"
-                src="/static/instagram.webp"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-              <Typography variant="caption">Instagram</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        {social && social.youtube && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://youtube.com/${social.youtube}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="youtube"
-                src="/static/youtube.webp"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-              <Typography variant="caption">Youtube</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        {social && social.medium && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://medium.com/${social.medium}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="medium"
-                src="/static/medium.webp"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-              <Typography variant="caption">Medium</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        {social && social.twitch && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://twitch.tv/${social.twitch}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="twitch"
-                src="/static/twitch.webp"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-              <Typography variant="caption">Twitch</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        {social && social.tiktok && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://tiktok.com/${social.tiktok}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="tiktok"
-                src="/static/tiktok.webp"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-              <Typography variant="caption">Tiktok</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        {social && social.reddit && (
-          <Link
-            underline="none"
-            color="inherit"
-            target="_blank"
-            href={`https://www.reddit.com/${social.reddit}`}
-            rel="noreferrer noopener nofollow"
-          >
-            <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-              <Avatar
-                alt="reddit"
-                src="/static/reddit.svg"
-                sx={{ mr: '5px', width: 16, height: 16 }}
-              />
-              <Typography variant="caption">Reddit</Typography>
-            </MenuItem>
-          </Link>
-        )}
-        <Link
-          underline="none"
-          color="inherit"
-          target="_blank"
-          href={`https://www.xrpchat.com/`}
-          rel="noreferrer noopener nofollow"
-        >
-          <MenuItem divider={true} sx={{ py: 1, px: 2 }}>
-            <Avatar
-              alt="xrpchat"
-              src="/static/xrpchat.webp"
-              sx={{ mr: '5px', width: 16, height: 16 }}
-            />
-            <Typography variant="caption">XRP Chat</Typography>
-          </MenuItem>
-        </Link>
+        {renderIconItem('XRPChat', personFill)}
       </Box>
     </Drawer>
   );
