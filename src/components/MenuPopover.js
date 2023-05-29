5e1ba798e6dbb1a09b85ddacb7859574c6eab337
Yannier <93131737+yannierlesende@users.noreply.github.com>
2023-05-28 20:38:31 -0400

    Update MenuPopover.js
    import PropTypes directly from the 'prop-types' package instead of the 'react' package.
    
    Destructure props: Instead of accessing children, sx, and ...other individually, you can destructure them directly in the function's parameter.
    
    Use object shorthand notation: Instead of explicitly defining children: PropTypes.node.isRequired and sx: PropTypes.object in the propTypes declaration, you can use object shorthand notation.
    
    Remove unnecessary comments: Commented code like the boxShadow line can be removed to improve code clarity.

--- src/components/MenuPopover.js
+++ src/components/MenuPopover.js
@@ -1,37 +1,31 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-
-// Material
 import { Popover } from '@mui/material';
-import {
-    alpha,
-    styled
-} from '@mui/material';
+import { alpha, styled } from '@mui/material';
 
-// ----------------------------------------------------------------------
 MenuPopover.propTypes = {
-    children: PropTypes.node.isRequired,
-    sx: PropTypes.object
+  children: PropTypes.node.isRequired,
+  sx: PropTypes.object,
 };
 
 export default function MenuPopover({ children, sx, ...other }) {
-    return (
-        <Popover
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            PaperProps={{
-                sx: {
-                    mt: 1.5,
-                    ml: 0.5,
-                    overflow: 'inherit',
-                    // boxShadow: (theme) => theme.customShadows.z20,
-                    border: (theme) => `solid 1px ${alpha('#919EAB', 0.08)}`,
-                    width: 200,
-                    ...sx
-                }
-            }}
-            {...other}
-        >
-            {children}
-        </Popover>
-    );
+  return (
+    <Popover
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      PaperProps={{
+        sx: {
+          mt: 1.5,
+          ml: 0.5,
+          overflow: 'inherit',
+          border: (theme) => `solid 1px ${alpha('#919EAB', 0.08)}`,
+          width: 200,
+          ...sx,
+        },
+      }}
+      {...other}
+    >
+      {children}
+    </Popover>
+  );
 }
