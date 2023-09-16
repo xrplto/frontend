0c080f25b5f20317fc6e5502e2e571b6fc0774e8
Yannier Lesende <webxtor@ukr.net>
2023-09-17 00:20:01 +0200

    NumberTooltip component >1 big numbers & append

--- src/components/NumberTooltip.js
+++ src/components/NumberTooltip.js
@@ -1,18 +1,21 @@
 import { Tooltip } from '@mui/material';
+// Utils
+import { fNumber } from 'src/utils/formatNumber';
 
 
-
-export default function NumberTooltip({ number, prepend = '', pos = 'top' }) {
+export default function NumberTooltip({ number, prepend = '', append = '', pos = 'top' }) {
 	let numberTrim = number;
 	if (numberTrim < 1 && numberTrim.toString().length > 10 && !numberTrim.toString().includes('e')) {
 		//console.log('NumberTooltip', number);
 		numberTrim = '0.0...' + number.slice(-4);
+	} else if(numberTrim > 1 && numberTrim.toString().split('.')[0].length > 10 && !numberTrim.toString().includes('e')) {
+		numberTrim = number.slice(0, 2) + '...'  + number.slice(-4);
 	} else {
-		return number;// if small enough return untrimmed
+		return prepend + fNumber(number) + append;// if small enough return untrimmed
 	}
 	return (
-		<Tooltip title={prepend + number} placement={pos} arrow>
-		  <span>{prepend}{numberTrim}</span>
+		<Tooltip title={prepend + fNumber(number) + append} placement={pos} arrow>
+		  <span>{prepend}{numberTrim}{append}</span>
 		</Tooltip>
 	);
 }
