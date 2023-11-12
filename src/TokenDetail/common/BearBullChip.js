e7bbc9faee0a6f54f24f88164f12e7a77a56a82e
XRPL.to <93736363+xrplto@users.noreply.github.com>
2023-11-11 19:47:47 -0500

    Update BearBullChip.js
    Made the styling of the Chips more squared / optimized code.

--- src/TokenDetail/common/BearBullChip.js
+++ src/TokenDetail/common/BearBullChip.js
@@ -1,78 +1,55 @@
 import PropTypes from 'prop-types';
-// Material
 import { withStyles } from '@mui/styles';
-import {
-    Chip,
-    Tooltip,
-    Typography
-} from '@mui/material';
-
-// Iconify
+import { Chip, Tooltip, Typography } from '@mui/material';
 import { Icon } from '@iconify/react';
 import caretDown from '@iconify/icons-bx/caret-down';
 import caretUp from '@iconify/icons-bx/caret-up';
-
-// Utils
 import { fPercent } from 'src/utils/formatNumber';
 
-// ----------------------------------------------------------------------
+const BearBullChip = ({ value, tooltip }) => {
+  const pro = fPercent(value);
+  const isBearish = pro < 0;
+  const strPro = `${isBearish ? -pro : pro} %`;
+
+  const ChipComponent = isBearish ? BearishChip : BullishChip;
+
+  return (
+    <Tooltip title={tooltip}>
+      <ChipComponent
+        icon={<Icon icon={isBearish ? caretDown : caretUp} width="16" height="16" color="#fff" />}
+        size="small"
+        label={
+          <WhiteTextTypography variant="subtitle2" style={{ color: '#FFFFFF !important' }}>
+            {strPro}
+          </WhiteTextTypography>
+        }
+        style={{ borderRadius: '8px' }}
+      />
+    </Tooltip>
+  );
+};
+
+BearBullChip.propTypes = {
+  value: PropTypes.number,
+  tooltip: PropTypes.any,
+};
+
 const BearishChip = withStyles({
-    root: {
-        backgroundColor: "#B72136 !important"
-    }
+  root: {
+    backgroundColor: '#B72136 !important',
+  },
 })(Chip);
 
 const BullishChip = withStyles({
-    root: {
-        backgroundColor: "#007B55 !important"
-    }
+  root: {
+    backgroundColor: '#007B55 !important',
+  },
 })(Chip);
 
 const WhiteTextTypography = withStyles({
-    root: {
-        color: "#FFFFFF !important"
-    }
+  root: {
+    color: '#FFFFFF !important',
+  },
 })(Typography);
 
-BearBullChip.propTypes = {
-    value: PropTypes.number,
-    tooltip: PropTypes.any
-};
-
-function abs(num) {
-    if (num < 0)
-        return -num;
-    return num;
-}
-
-export default function BearBullChip({value, tooltip}) {
-    let pro = fPercent(value);
-
-    if (abs(pro) < 0.0001) pro = 0;
-
-    let strPro = 0;
-    if (pro < 0) {
-        strPro = -pro;
-        strPro = strPro + ' %';
-    } else {
-        strPro = pro + ' %';
-    }
-    return (
-        <Tooltip title={tooltip}>
-            {pro < 0 ? (
-                <BearishChip
-                    icon={<Icon icon={caretDown} width="16" height="16" color='#fff'/>}
-                    size="small"
-                    label={<WhiteTextTypography variant="subtitle2">{strPro}</WhiteTextTypography>}
-                />
-            ) : (
-                <BullishChip
-                    icon={<Icon icon={caretUp} width="16" height="16" color='#fff'/>}
-                    size="small"
-                    label={<WhiteTextTypography variant="subtitle2">{strPro}</WhiteTextTypography>}
-                />
-            )}
-        </Tooltip>
-    );
-}
-
+export default BearBullChip;
