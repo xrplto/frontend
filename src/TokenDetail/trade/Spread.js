a716ea44da118b20c7999dc0b341b2fa4870fa3b
XRPL.to <93736363+xrplto@users.noreply.github.com>
2023-11-17 09:38:26 -0500

    Update Spread.js
    Updated spread calculating formula.

--- src/TokenDetail/trade/Spread.js
+++ src/TokenDetail/trade/Spread.js
@@ -1,47 +1,47 @@
 import React from 'react';
-// Material
-import {
-    Tooltip,
-    Typography
-} from '@mui/material';
+// Material UI components
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 
-// Iconify
+// Iconify for icons
 import { Icon } from '@iconify/react';
 import infoFilled from '@iconify/icons-ep/info-filled';
 
-const formatNumber = (arg) => {
-    return new Intl.NumberFormat('en-US').format(arg);
-};
+// Function to format numbers in 'en-US' locale
+const formatNumber = (number) => new Intl.NumberFormat('en-US').format(number);
 
 const Spread = ({ bids, asks }) => {
-    const getHighestBid = (bids) => {
-        const prices = bids.map(bid => bid.price);
-        return  Math.max.apply(Math, prices);
-    }
+    // Function to get the highest bid price
+    const getHighestBid = (bids) => Math.max(...bids.map(bid => bid.price));
 
-    const getLowestAsk = (asks) => {
-        const prices = asks.map(ask => ask.price);
-        return  Math.min.apply(Math, prices);
-    }
+    // Function to get the lowest ask price
+    const getLowestAsk = (asks) => Math.min(...asks.map(ask => ask.price));
 
+    // Function to calculate the spread amount
     const getSpreadAmount = (bids, asks) => Math.abs(getHighestBid(bids) - getLowestAsk(asks));
 
-    const getSpreadPercentage = (spread, highestBid) => `(${((spread * 100) / highestBid).toFixed(2)}%)`;
+    // Function to calculate spread percentage
+    const getSpreadPercentage = (spread, highestBid) => `(${((spread / highestBid) * 100).toFixed(2)}%)`;
 
-    /*
-    The bid–ask spread (also bid–offer or bid/ask and buy/sell in the case of a market maker) 
-    is the difference between the prices quoted (either by a single market maker or in a limit order book) 
-    for an immediate sale (ask) and an immediate purchase (bid) for stocks, futures contracts, options, or currency pairs.
-    */
+    // Calculate spread amount and spread percentage
+    const spreadAmount = getSpreadAmount(bids, asks);
+    const spreadPercentage = getSpreadPercentage(spreadAmount, getHighestBid(bids));
 
     return (
         <>
-        <Typography variant='subtitle1' sx={{color:'#9E86FF', ml:0, mt:2, mb:1}}>
-            Spread: {formatNumber(getSpreadAmount(bids, asks))} {getSpreadPercentage(getSpreadAmount(bids, asks), getHighestBid(bids))}
-        </Typography>
-        <Tooltip title={<Typography style={{display: 'inline-block'}} variant="body2">The bid-ask spread, also known as bid-offer or bid/ask (and buy/sell when referring to a market maker), represents the difference between the quoted prices for an immediate sale (ask) and an immediate purchase (bid) of financial instruments such as stocks, futures contracts, options, or currency pairs. These prices can be quoted by a single market maker or found in a limit order book.</Typography>}>
-            <Icon icon={infoFilled} />
-        </Tooltip>
+            <Typography variant='subtitle1' sx={{ color: '#9E86FF', ml: 0, mt: 2, mb: 1 }}>
+                Spread: {formatNumber(spreadAmount)} {spreadPercentage}
+            </Typography>
+            <Tooltip title={
+                <Typography variant="body2">
+                    The bid-ask spread, also known as bid-offer or bid/ask (and buy/sell when referring to a market maker),
+                    represents the difference between the quoted prices for an immediate sale (ask) and an immediate purchase (bid)
+                    of financial instruments such as stocks, futures contracts, options, or currency pairs.
+                    These prices can be quoted by a single market maker or found in a limit order book.
+                </Typography>
+            }>
+                <Icon icon={infoFilled} />
+            </Tooltip>
         </>
     );
 };
