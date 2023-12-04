1daefa748b1ae578fcce293040a66861fdba7a1f
CoderBoy M J H Nahid <112113447+CoderBoyMJHNahid@users.noreply.github.com>
2023-12-04 20:54:27 +0600

    Drop Down menu Hover fixed code
    Updated some code on the zMain.css file and also Header.js. Add a new file DropDownMenu.js to the component folder

---
+++ DropDownMenu.js
@@ -0,0 +1,138 @@
+import { Link as MuiLink } from '@mui/material';
+import {
+  styled,
+  Link,
+  Menu,
+  useTheme,
+  MenuItem
+} from '@mui/material';
+// Icon Imports
+import BarChartIcon from '@mui/icons-material/BarChart';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import WhatshotIcon from '@mui/icons-material/Whatshot'; // Gainers & Losers
+import VisibilityIcon from '@mui/icons-material/Visibility'; // Most Viewed
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'; // Recently Added
+import StarIcon from '@mui/icons-material/Star'; // Spotlight
+import { useContext } from 'react';
+import { AppContext } from 'src/AppContext';
+import { useTranslation } from 'react-i18next';
+
+const StyledLink = styled(Link)(
+  ({ darkMode }) => `
+      font-weight: 700;
+      margin-right: 27px;
+      padding: 8px 10px; // Increased padding
+      border-radius: 8px;
+      transition: background-color 0.3s;
+      display: inline-block; // Ensure it's not inline
+      &:hover {
+        color: ${darkMode ? '#005E46' : '#4455CC'};
+        cursor: pointer;
+      }
+  `
+);
+
+
+const DropDownMenu = ({ handleMenuOpen, handleMenuClose, anchorEl, open }) => {
+  const { t } = useTranslation();
+  const theme = useTheme();
+  const { darkMode, setDarkMode } = useContext(AppContext);
+
+  return (
+    <>
+      <ul id="dropdown_menu">
+        <li>
+          <StyledLink
+            underline="none"
+            color={darkMode ? 'white' : 'black'}
+            sx={{
+              '&:hover': {
+                color: darkMode ? '#22B14C !important' : '#3366FF !important'
+              }
+            }}
+            href="/"
+          >
+            {t('Tokens')}
+
+            <ul id="dropItems" style={{
+              background: darkMode ? 'rgb(23, 23, 26)' : 'rgb(244, 245, 251)',
+              border: darkMode ? '1px solid rgb(33, 37, 43)' : '1px solid rgb(220, 220, 220)',
+              color: darkMode ? 'white' : '#343434',
+              }}>
+              <li id={darkMode ? 'darkItem' : 'lightItem'}>
+                  <MuiLink 
+                    href="/"
+                    color="inherit"
+                    underline="none"
+                    sx={{ display: 'flex', alignItems: 'center' }}
+                  >
+                      <BarChartIcon sx={{ marginRight: theme.spacing(1) }} />
+                      Ranking
+                  </MuiLink>
+              </li>
+              <li id={darkMode ? 'darkItem' : 'lightItem'}>
+                  <MuiLink
+                    href="/new"
+                    color="inherit"
+                    underline="none"
+                    sx={{ display: 'flex', alignItems: 'center' }}
+                  >
+                    <AddCircleOutlineIcon
+                      sx={{ marginRight: theme.spacing(1) }}
+                    />
+                    Recently Added
+                  </MuiLink>
+              </li>
+              <li id={darkMode ? 'darkItem' : 'lightItem'}>
+                  <MuiLink
+                    href="/best-tokens"
+                    color="inherit"
+                    underline="none"
+                    sx={{ display: 'flex', alignItems: 'center' }}
+                  >
+                    <StarIcon sx={{ marginRight: theme.spacing(1) }} />
+                    Spotlight
+                  </MuiLink>
+              </li>
+              <li id={darkMode ? 'darkItem' : 'lightItem'}>
+                  <MuiLink
+                    href="/trending-tokens"
+                    color="inherit"
+                    underline="none"
+                    sx={{ display: 'flex', alignItems: 'center' }}
+                  >
+                    <TrendingUpIcon sx={{ marginRight: theme.spacing(1) }} />
+                    Trending
+                  </MuiLink>
+              </li>
+              <li id={darkMode ? 'darkItem' : 'lightItem'}>
+                  <MuiLink
+                    href="/gainers-losers"
+                    color="inherit"
+                    underline="none"
+                    sx={{ display: 'flex', alignItems: 'center' }}
+                  >
+                    <WhatshotIcon sx={{ marginRight: theme.spacing(1) }} />
+                    Gainers & Losers
+                  </MuiLink>
+              </li>
+              <li id={darkMode ? 'darkItem' : 'lightItem'}>
+                  <MuiLink
+                    href="/most-viewed-tokens"
+                    color="inherit"
+                    underline="none"
+                    sx={{ display: 'flex', alignItems: 'center' }}
+                  >
+                    <VisibilityIcon sx={{ marginRight: theme.spacing(1) }} />
+                    Most Viewed
+                  </MuiLink>
+              </li>
+            </ul>
+          </StyledLink>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default DropDownMenu;
---
+++ Header.js
@@ -0,0 +1,224 @@
+import { useState, useContext } from 'react';
+import 'src/utils/i18n';
+import { useTranslation } from 'react-i18next';
+import {
+  alpha,
+  styled,
+  useMediaQuery,
+  useTheme,
+  Box,
+  Container,
+  IconButton,
+  Link,
+  Stack,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link as MuiLink } from '@mui/material'; // Importing MUI Link
+
+// Context
+import { AppContext } from 'src/AppContext';
+
+// Components
+import Logo from 'src/components/Logo';
+import Wallet from 'src/components/Wallet';
+import NavSearchBar from './NavSearchBar';
+import SidebarDrawer from './SidebarDrawer';
+import ThemeSwitcher from './ThemeSwitcher';
+import DropDownMenu from './DropDownMenu';
+
+const HeaderWrapper = styled(Box)(
+  ({ theme }) => `
+    width: 100%;
+    display: flex;
+    align-items: center;
+    height: ${theme.spacing(10)};
+    margin-bottom: ${theme.spacing(0)};
+    border-radius: 0px;
+    border-bottom: 1px solid ${alpha('#CBCCD2', 0.2)};
+`
+);
+
+const StyledLink = styled(Link)(
+  ({ darkMode }) => `
+    font-weight: 700;
+    margin-right: 27px;
+    padding: 8px 10px; // Increased padding
+    border-radius: 8px;
+    transition: background-color 0.3s;
+    display: inline-block; // Ensure it's not inline
+    &:hover {
+      color: ${darkMode ? '#005E46' : '#4455CC'};
+      cursor: pointer;
+    }
+`
+);
+
+const MenuContainer = styled('div')({
+  position: 'relative',
+  display: 'inline-block' // Make sure this is an inline-block to properly wrap content
+});
+
+export default function Header(props) {
+  const { t } = useTranslation(); // set translation const
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.down('md'));
+  const [fullSearch, setFullSearch] = useState(false);
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const { darkMode, setDarkMode } = useContext(AppContext);
+
+  const handleFullSearch = (e) => {
+    setFullSearch(true);
+  };
+
+  const toggleDrawer = (isOpen = true) => {
+    setOpenDrawer(isOpen);
+  };
+
+  // Dropdown menu state and functions
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <HeaderWrapper>
+      <Container maxWidth="xl">
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          flex={2}
+          sx={{ pl: 0, pr: 0 }}
+        >
+          <Box
+            id="logo-container-laptop"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', sm: 'flex' },
+              alignItems: 'center'
+            }}
+          >
+            <Logo alt="xrpl.to Logo" style={{ marginRight: 25 }} />
+
+            {!isTablet && (
+              <>
+                <MenuContainer
+                  onMouseLeave={handleMenuClose}
+                  onMouseEnter={handleMenuOpen}
+                >
+                  <DropDownMenu 
+                    handleMenuOpen
+                    handleMenuClose
+                    anchorEl
+                    open
+                  />
+
+                </MenuContainer>
+                <StyledLink
+                  underline="none"
+                  color={darkMode ? 'white' : 'black'}
+                  sx={{
+                    '&:hover': {
+                      color: darkMode
+                        ? '#22B14C !important'
+                        : '#3366FF !important'
+                    }
+                  }}
+                  href="/swap"
+                >
+                  {t('Swap')}
+                </StyledLink>
+                <StyledLink
+                  underline="none"
+                  color={darkMode ? 'white' : 'black'}
+                  sx={{
+                    '&:hover': {
+                      color: darkMode
+                        ? '#22B14C !important'
+                        : '#3366FF !important'
+                    }
+                  }}
+                  href="/buy-xrp"
+                >
+                  {t('Fiat')}
+                </StyledLink>
+              </>
+            )}
+          </Box>
+
+          {fullSearch && (
+            <NavSearchBar
+              id="id_search_tokens"
+              placeholder="Search XRPL Tokens"
+              fullSearch={fullSearch}
+              setFullSearch={setFullSearch}
+            />
+          )}
+
+          {!fullSearch && (
+            <Box
+              id="logo-container-mobile"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', sm: 'none' }
+              }}
+            >
+              <Logo alt="xrpl.to Logo" />
+            </Box>
+          )}
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'center'
+            }}
+          >
+            {!fullSearch && !isTablet && (
+              <Stack mr={2}>
+                <NavSearchBar
+                  id="id_search_tokens"
+                  placeholder="Search XRPL Tokens"
+                  fullSearch={fullSearch}
+                  setFullSearch={setFullSearch}
+                />
+              </Stack>
+            )}
+
+            {!fullSearch && isTablet && (
+              <IconButton aria-label="search" onClick={handleFullSearch}>
+                <SearchIcon />
+              </IconButton>
+            )}
+
+            {!fullSearch && !isTablet && (
+              <Wallet style={{ marginRight: '5px' }} />
+            )}
+
+            {!isTablet && (
+              <ThemeSwitcher darkMode={darkMode} setDarkMode={setDarkMode} />
+            )}
+
+            {isTablet && !fullSearch && (
+              <IconButton onClick={() => toggleDrawer(true)}>
+                <MenuIcon />
+              </IconButton>
+            )}
+
+            <SidebarDrawer toggleDrawer={toggleDrawer} isOpen={openDrawer} />
+          </Box>
+        </Box>
+      </Container>
+    </HeaderWrapper>
+  );
+}
