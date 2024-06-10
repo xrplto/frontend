0f3a15b8910f0a98c83d4531724547f4ce7601a0
climax-solution <blocklimax@gmail.com>
2024-06-10 19:48:43 +0300

    fix favicon issue

--- pages/_app.js
+++ pages/_app.js
@@ -71,8 +71,8 @@ function XRPLToApp({ Component, pageProps, router }) {
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#ffffff"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/webp" sizes="32x32" href="/icons/favicon-32x32.webp" />
+        <link rel="icon" type="image/webp" sizes="16x16" href="/icons/favicon-16x16.webp" />
         <link rel="manifest" href="/icons/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
         <link rel="shortcut icon" href="/icons/favicon.ico" type="image/x-icon" />
