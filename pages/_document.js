896f386c75596e3fa67dbe879a962e0398f093a0
XRPL.to <93736363+xrplto@users.noreply.github.com>
2024-07-19 07:14:55 -0400

    removed google tag and loading fonts from google.

--- pages/_document.js
+++ pages/_document.js
@@ -2,34 +2,11 @@ import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-    // Added a method to insert Google Tag Manager script for cleaner code
-    setGoogleTags() {
-        return {
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-PHYSGW6VJ9', { page_path: window.location.pathname });
-            `,
-        };
-    }
-
     render() {
         return (
             <Html lang="en" dir="ltr">
                 <Head>
-                    {/* Using preconnect to improve loading performance for Google Fonts */}
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-                    />
-
-                    {/* Async loading of Google Tag Manager */}
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PHYSGW6VJ9"/>
-
-                    {/* Using method to set Google Tag Manager script */}
-                    <script dangerouslySetInnerHTML={this.setGoogleTags()} />
+                    
                 </Head>
                 <body>
                     <Main />
