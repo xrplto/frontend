d6b670a398b39507298282b17d5b7c851778598c
XRPL.to <93736363+xrplto@users.noreply.github.com>
2024-07-23 06:08:49 -0400

    Fix search - Implemented link on tokens - Added scroll on results - Fixed bug if double clicked tab results disppear.

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -4,6 +4,7 @@ import CloseIcon from '@mui/icons-material/Close';
 import CasinoIcon from '@mui/icons-material/Casino';
 import AnimationIcon from '@mui/icons-material/Animation';
 import VerifiedIcon from '@mui/icons-material/Verified';
+import Tooltip from '@mui/material/Tooltip';
 
 import { useContext, useEffect, useRef, useState } from "react";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
@@ -42,7 +43,6 @@ const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
     },
 }));
 
-
 function truncate(str, n) {
     if (!str) return '';
     return str.length > n ? str.substr(0, n - 1) + '... ' : str;
@@ -73,7 +73,6 @@ const NFTRender = ({
     useEffect(() => {
         initOption()
     }, [slug])
-
 
     return (
         <Link
@@ -253,7 +252,9 @@ export default function SearchModal({ onClose, open }) {
                     value={activeTab}
                     exclusive
                     onChange={(_, newValue) => {
-                        setActiveTab(newValue);
+                        if (newValue !== null) {
+                            setActiveTab(newValue);
+                        }
                     }}
                     aria-label="text formatting"
                 >
@@ -278,71 +279,71 @@ export default function SearchModal({ onClose, open }) {
                         >
                             <path d="M17.0881 9.42254C16.4368 8.90717 15.8155 8.35512 15.3012 7.71336C12.3755 4.06357 13.8912 1 13.8912 1C8.46026 3.18334 7.22337 6.64895 7.16462 9.22981L7.1675 9.2572C7.1675 9.2572 7.21498 10.7365 7.90791 12.3625C8.12481 12.8713 7.88299 13.4666 7.33195 13.6199C6.87638 13.7465 6.40822 13.5317 6.21571 13.1314C5.90413 12.4831 5.49262 11.4521 5.6109 10.7249C4.75064 11.817 4.1815 13.1452 4.03542 14.6184C3.65092 18.4924 6.43759 22.0879 10.4208 22.8488C14.9906 23.7217 19.3121 20.7182 19.9269 16.3623C20.3117 13.6367 19.1498 11.0538 17.0881 9.42254ZM14.3578 17.7393C14.3289 17.776 13.5893 18.6597 12.3501 18.7517C12.2829 18.7547 12.2124 18.7577 12.1452 18.7577C11.2902 18.7577 10.4226 18.3682 9.56103 17.5951L9.37219 17.4262L9.61243 17.3372C9.62843 17.3312 11.2742 16.7236 11.6778 15.4077C11.8155 14.9629 11.7707 14.4566 11.553 13.9842C11.2905 13.4075 10.7845 11.9564 11.7453 10.9041L11.9309 10.7015L12.0206 10.9561C12.0238 10.9714 12.6034 12.5911 13.9741 13.4379C14.3871 13.6957 14.6977 14.0086 14.8931 14.3644C15.2959 15.1132 15.533 16.3065 14.3578 17.7393Z" />
                         </svg>
-
                     </Stack>
-                    <MenuList sx={{ px: 0 }}>
-                        {tokens.slice(0, activeTab == "token" ? tokens.length : 3).map(({ md5, name, slug, isOMCF, user, kyc, pro24h, exch }, idx) => {
+                    <MenuList sx={{ px: 0, height: "300px", overflowY: "auto" }}>
+                        {tokens.slice(0, activeTab == "token" ? tokens.length : 4).map(({ md5, name, slug, isOMCF, user, kyc, pro24h, exch }, idx) => {
                             const imgUrl = `https://s1.xrpl.to/token/${md5}`;
                             const link = `/token/${slug}?fromSearch=1`;
 
                             return (
-                                <MenuItem sx={{ py: "2px", px: 1, height: "50px" }} key={idx}>
-                                    <Box
-                                        display="flex"
-                                        alignItems="center"
-                                        justifyContent="space-between"
-                                        flex={2}
-                                        sx={{ pl: 0, pr: 0 }}
-                                    >
-                                        <Stack direction="row" spacing={1} alignItems="center">
-                                            <TokenImage
-                                                src={imgUrl}
-                                                width={24}
-                                                height={24}
-                                            // onError={(event) => (event.target.src = '/static/alt.webp')}
-                                            />
-                                            <Stack>
-                                                <Typography
-                                                    variant="token"
-                                                    color={isOMCF !== 'yes'
-                                                        ? darkMode
-                                                            ? '#fff'
-                                                            : '#222531'
-                                                        : darkMode
-                                                            ? '#007B55'
-                                                            : '#5569ff'}
-                                                    noWrap
-                                                >
-                                                    {truncate(user, 8)}
-                                                </Typography>
-                                                <Typography
-                                                    variant="caption"
-                                                    color={isOMCF !== 'yes' ? (darkMode ? '#fff' : '#222531') : ''}
-                                                    noWrap
-                                                >
-                                                    {truncate(name, 13)}
-                                                    {kyc && (
-                                                        <Typography variant="kyc" sx={{ ml: 0.2 }}>
-                                                            KYC
-                                                        </Typography>
-                                                    )}
-                                                </Typography>
+                                <Link key={idx} href={link} underline="none" color="inherit">
+                                    <MenuItem sx={{ py: "2px", px: 1, height: "50px" }}>
+                                        <Box
+                                            display="flex"
+                                            alignItems="center"
+                                            justifyContent="space-between"
+                                            flex={2}
+                                            sx={{ pl: 0, pr: 0 }}
+                                        >
+                                            <Stack direction="row" spacing={1} alignItems="center">
+                                                <TokenImage
+                                                    src={imgUrl}
+                                                    width={24}
+                                                    height={24}
+                                                />
+                                                <Stack>
+                                                    <Typography
+                                                        variant="token"
+                                                        color={isOMCF !== 'yes'
+                                                            ? darkMode
+                                                                ? '#fff'
+                                                                : '#222531'
+                                                            : darkMode
+                                                                ? '#007B55'
+                                                                : '#5569ff'}
+                                                        noWrap
+                                                    >
+                                                        {truncate(user, 8)}
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="caption"
+                                                        color={isOMCF !== 'yes' ? (darkMode ? '#fff' : '#222531') : ''}
+                                                        noWrap
+                                                    >
+                                                        {truncate(name, 13)}
+                                                        {kyc && (
+                                                            <Typography variant="kyc" sx={{ ml: 0.2 }}>
+                                                                KYC
+                                                            </Typography>
+                                                        )}
+                                                    </Typography>
+                                                </Stack>
                                             </Stack>
-                                        </Stack>
 
-                                        <Stack direction="row" gap={1}>
-                                            <NumberTooltip
-                                                prepend={currencySymbols[activeFiatCurrency]}
-                                                number={fNumberWithCurreny(exch, exchRate)}
-                                            />
-                                            <BearBullLabel value={pro24h} variant="h4" />
-                                        </Stack>
-                                    </Box>
-                                </MenuItem>
-                            )
+                                            <Stack direction="row" gap={1}>
+                                                <NumberTooltip
+                                                    prepend={currencySymbols[activeFiatCurrency]}
+                                                    number={fNumberWithCurreny(exch, exchRate)}
+                                                />
+                                                <BearBullLabel value={pro24h} variant="h4" />
+                                            </Stack>
+                                        </Box>
+                                    </MenuItem>
+                                </Link>
+                            );
                         })}
                     </MenuList>
-                    <Button onClick={() => setActiveTab("token")} sx={{ display: (search && activeTab != "token") ? "flex" : "none"}}>See all results &#40;{tokens.length}&#41;</Button>
+                    <Button onClick={() => setActiveTab("token")} sx={{ display: (search && activeTab != "token") ? "flex" : "none", mt: -10 }}>See all results &#40;{tokens.length}&#41;</Button>
                 </Stack>
             }
 
@@ -364,14 +365,14 @@ export default function SearchModal({ onClose, open }) {
                         </svg>
 
                     </Stack>
-                    <MenuList sx={{ px: 0 }}>
+                    <MenuList sx={{ px: 0, height: "220px", overflowY: "auto" }}>
                         {
                             collections.slice(0, activeTab == "nft" ? collections.length : 3).map((nft, idx) => (
                                 <NFTRender key={idx} {...nft} darkMode={darkMode} />
                             ))
                         }
                     </MenuList>
-                    <Button onClick={() => setActiveTab("nft")} sx={{ display: (search && activeTab != "nft") ? "flex" : "none"}}>See all results &#40;{collections.length}&#41;</Button>
+                    <Button onClick={() => setActiveTab("nft")} sx={{ display: (search && activeTab != "nft") ? "flex" : "none", mt: -10 }}>See all results &#40;{collections.length}&#41;</Button>
                 </Stack>
             }
         </Paper>
