7c83ee9a8dcf09c35132062b46e89e25553e2f2b
XRPL.to <93736363+xrplto@users.noreply.github.com>
2024-10-15 06:03:24 -0400

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,27 @@
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en" dir="ltr">
-                <Head>
-                    
-                </Head>
+                <Head />
                 <body>
                     <Main />
                     <NextScript />
+                    <Script
+                        src="https://www.googletagmanager.com/gtag/js?id=G-3V4R50SVNH"
+                        strategy="afterInteractive"
+                    />
+                    <Script id="google-analytics" strategy="afterInteractive">
+                        {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-3V4R50SVNH');
+                        `}
+                    </Script>
                 </body>
             </Html>
         );
