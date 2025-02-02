954d72b780063a1982fc5fddb0ab233c329ae4ba
xrplto <hello@xrpl.to>
2025-02-02 10:17:04 -0500

    ui update

--- src/components/CryptoHeatmap.js
+++ src/components/CryptoHeatmap.js
@@ -17,16 +17,18 @@ function CryptoHeatmap({ exchRate }) {
   const [markets, setMarkets] = useState([]);
 
   useEffect(() => {
-    const getTokenData = async() => {
+    const getTokenData = async () => {
       try {
         const BASE_URL = process.env.API_URL;
-        const res = await axios.get(`${BASE_URL}/tokens?start=1&limit=100&sortBy=vol24hxrp&sortType=desc&filter=&tags=yes&showNew=false&showSlug=false`);
+        const res = await axios.get(
+          `${BASE_URL}/tokens?start=1&limit=100&sortBy=vol24hxrp&sortType=desc&filter=&tags=yes&showNew=false&showSlug=false`
+        );
 
         let data = res.data;
         if (data) {
           const tokens = data.tokens;
           let marketData = [];
-          
+
           tokens.map((token) => {
             const market = {
               name: token.name,
@@ -35,26 +37,30 @@ function CryptoHeatmap({ exchRate }) {
               priceChange: token.pro24h,
               price: token.exch,
               slug: token.slug,
-              color: token.pro24h >= 0 ? "#16c784" : "#a4111a"
+              color: token.pro24h >= 0 ? '#16c784' : '#a4111a',
+              trustlines: token.trustlines,
+              holders: token.holders,
+              verified: token.verified,
+              kyc: token.kyc
             };
             marketData.push(market);
           });
-      
+
           setMarkets(marketData);
         }
-      } catch(err) {
+      } catch (err) {
         console.log(err);
       }
-    }
+    };
 
     getTokenData();
-  }, [])
+  }, []);
 
   const options = {
     series: [
       {
         type: 'treemap',
-        cursor: "pointer",
+        cursor: 'pointer',
         layoutAlgorithm: 'squarified',
         data: markets,
         dataLabels: {
@@ -64,15 +70,31 @@ function CryptoHeatmap({ exchRate }) {
           formatter: function () {
             const marketcap = Decimal.div(this.point.value, exchRate).toNumber();
             if (this.point.shapeArgs.width > this.point.shapeArgs.height) {
-              return `<div style="color: #fff;"> <div style="text-align: center; font-size:${(this.point.shapeArgs.height) / 10}px;">${this.key}</div><div style="text-align: center; font-size:${(this.point.shapeArgs.height) / 12}px;">${currencySymbols[activeFiatCurrency]} ${fCurrency(marketcap)}</div><div style="text-align: center; font-size:${(this.point.shapeArgs.height) / 12}px;">${fCurrency(this.point.priceChange)}% </div></div>`;
+              return `<div style="color: #fff;"> <div style="text-align: center; font-size:${
+                this.point.shapeArgs.height / 10
+              }px;">${this.key}</div><div style="text-align: center; font-size:${
+                this.point.shapeArgs.height / 12
+              }px;">${currencySymbols[activeFiatCurrency]} ${fCurrency(
+                marketcap
+              )}</div><div style="text-align: center; font-size:${
+                this.point.shapeArgs.height / 12
+              }px;">${fCurrency(this.point.priceChange)}% </div></div>`;
             } else {
-              return `<div style="color: #fff;"> <div style="text-align: center; font-size:${(this.point.shapeArgs.height) / 10}px;">${this.key}</div><div style="text-align: center; font-size:${(this.point.shapeArgs.height) / 12}px;">${currencySymbols[activeFiatCurrency]} ${fCurrency(marketcap)}</div><div style="text-align: center; font-size:${(this.point.shapeArgs.height) / 12}px;">${fCurrency(this.point.priceChange)}% </div></div>`;
+              return `<div style="color: #fff;"> <div style="text-align: center; font-size:${
+                this.point.shapeArgs.height / 10
+              }px;">${this.key}</div><div style="text-align: center; font-size:${
+                this.point.shapeArgs.height / 12
+              }px;">${currencySymbols[activeFiatCurrency]} ${fCurrency(
+                marketcap
+              )}</div><div style="text-align: center; font-size:${
+                this.point.shapeArgs.height / 12
+              }px;">${fCurrency(this.point.priceChange)}% </div></div>`;
             }
           }
         },
         events: {
-          click: function(event) {
-            window.location.href = "/token/" + event.point.slug;
+          click: function (event) {
+            window.location.href = '/token/' + event.point.slug;
           }
         }
       }
@@ -93,7 +115,27 @@ function CryptoHeatmap({ exchRate }) {
         zIndex: 100
       },
       formatter: function () {
-        return `<p style="color:#9ab;font-family:DobloxSans,sans-serif;border-radius:3px;font-size:11px;text-align:left;margin:0;padding:10px;border:1px solid black;background-color:#151519"><strong style="color:#99a5bb;font-weight:normal;font-size:14px;">${this.point.name}: ${this.point.original}<br> Price: ${currencySymbols[activeFiatCurrency]} ${fNumberWithCurreny(this.point.price, exchRate)}<br> Volume: ${fCurrency(this.point.value)}</strong></p>`;
+        const verifiedBadge = this.point.verified ? '✓ ' : '';
+        const kycBadge = this.point.kyc ? '🔒 ' : '';
+        const holders = this.point.holders ? this.point.holders.toLocaleString() : '0';
+        const trustlines = this.point.trustlines ? this.point.trustlines.toLocaleString() : '0';
+        const nameDisplay = this.point.original
+          ? `${this.point.name}: ${this.point.original}`
+          : this.point.name;
+
+        return `
+          <p style="color:#9ab;font-family:DobloxSans,sans-serif;border-radius:3px;font-size:11px;text-align:left;margin:0;padding:10px;border:1px solid black;background-color:#151519">
+            <strong style="color:#99a5bb;font-weight:normal;font-size:14px;">
+              ${verifiedBadge}${kycBadge}${nameDisplay}<br>
+              Price: ${currencySymbols[activeFiatCurrency]} ${fNumberWithCurreny(
+          this.point.price,
+          exchRate
+        )}<br>
+              Volume: ${fCurrency(this.point.value)}<br>
+              Holders: ${holders}<br>
+              Trustlines: ${trustlines}
+            </strong>
+          </p>`;
       }
     }
   };
@@ -104,11 +146,11 @@ function CryptoHeatmap({ exchRate }) {
       options={options}
       containerProps={{
         style: {
-          height: "calc(62.5em - 94px)"
+          height: 'calc(62.5em - 94px)'
         }
       }}
     />
-  )
+  );
 }
 
 export default CryptoHeatmap;
