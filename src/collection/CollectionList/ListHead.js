dcf319f48ad0bec7c800bfa49e33ce14b2b5df11
xrplto <hello@xrpl.to>
2025-02-03 14:00:42 -0500

    ui update

--- src/collection/CollectionList/ListHead.js
+++ src/collection/CollectionList/ListHead.js
@@ -1,144 +1,138 @@
 import { visuallyHidden } from '@mui/utils';
 import { withStyles } from '@mui/styles';
 import {
-    Box,
-    TableRow,
-    TableCell,
-    TableHead,
-    TableSortLabel,
-    useTheme,
-    useMediaQuery
+  Box,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableSortLabel,
+  useTheme,
+  useMediaQuery
 } from '@mui/material';
 
 const StickyTableCell = withStyles((theme) => ({
-    head: {
-        position: 'sticky',
-        zIndex: 1000,
-        top: 0
-    }
+  head: {
+    position: 'sticky',
+    zIndex: 1000,
+    top: 0
+  }
 }))(TableCell);
 
 const TABLE_HEAD = (isMobile, timeFrame) => {
-    const volumeLabel = timeFrame === '24h' ? 'Volume (24h)' : 'Volume (All)';
+  const volumeLabel = timeFrame === '24h' ? 'Volume (24h)' : 'Volume (All)';
 
-    if (isMobile) {
-        return [
-            {
-                no: 0,
-                id: 'name',
-                label: 'Collection',
-                align: 'left',
-                width: '40%',
-                order: false
-            },
-            {
-                no: 1,
-                id: 'volume',
-                label: volumeLabel,
-                align: 'right',
-                width: '30%',
-                order: true
-            },
-            {
-                no: 2,
-                id: 'floor.amount',
-                label: 'Floor',
-                align: 'right',
-                width: '30%',
-                order: true
-            }
-        ];
-    }
+  if (isMobile) {
     return [
-        {
-            no: 0,
-            id: 'name',
-            label: 'Collection',
-            align: 'left',
-            width: '40%',
-            order: false
-        },
-        {
-            no: 1,
-            id: 'floor.amount',
-            label: 'Floor',
-            align: 'right',
-            width: '20%',
-            order: true
-        },
-        {
-            no: 2,
-            id: 'volume',
-            label: volumeLabel,
-            align: 'right',
-            width: '10%',
-            order: true
-        },
-        {
-            no: 3,
-            id: 'owners',
-            label: 'Owners',
-            align: 'right',
-            width: '10%',
-            order: true
-        },
-        {
-            no: 4,
-            id: 'items',
-            label: 'Items',
-            align: 'right',
-            width: '10%',
-            order: true
-        }
+      {
+        no: 0,
+        id: 'name',
+        label: 'Collection',
+        align: 'left',
+        width: '40%',
+        order: false
+      },
+      {
+        no: 1,
+        id: 'volume',
+        label: volumeLabel,
+        align: 'right',
+        width: '30%',
+        order: true
+      },
+      {
+        no: 2,
+        id: 'floor.amount',
+        label: 'Floor',
+        align: 'right',
+        width: '30%',
+        order: true
+      }
     ];
+  }
+  return [
+    {
+      no: 0,
+      id: 'name',
+      label: 'Collection',
+      align: 'left',
+      width: '40%',
+      order: false
+    },
+    {
+      no: 1,
+      id: 'floor.amount',
+      label: 'Floor',
+      align: 'right',
+      width: '20%',
+      order: true
+    },
+    {
+      no: 2,
+      id: 'volume',
+      label: volumeLabel,
+      align: 'right',
+      width: '10%',
+      order: true
+    },
+    {
+      no: 3,
+      id: 'owners',
+      label: 'Owners',
+      align: 'right',
+      width: '10%',
+      order: true
+    },
+    {
+      no: 4,
+      id: 'items',
+      label: 'Items',
+      align: 'right',
+      width: '10%',
+      order: true
+    }
+  ];
 };
 
 export default function ListHead({ order, orderBy, onRequestSort, timeFrame }) {
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const createSortHandler = (id) => (event) => {
-        onRequestSort(event, id);
-    };
+  const createSortHandler = (id) => (event) => {
+    onRequestSort(event, id);
+  };
 
-    return (
-        <TableHead>
-            <TableRow style={{ background: '#00000000' }}>
-                {TABLE_HEAD(isMobile, timeFrame).map((headCell) => (
-                    <StickyTableCell
-                        key={headCell.id}
-                        align={headCell.align}
-                        sortDirection={orderBy === headCell.id ? order : false}
-                        width={headCell.width}
-                        sx={{
-                            ...(headCell.no > 0 && {
-                                pl: 0,
-                                pr: 0
-                            })
-                        }}
-                    >
-                        <TableSortLabel
-                            hideSortIcon
-                            active={orderBy === headCell.id}
-                            direction={orderBy === headCell.id ? order : 'desc'}
-                            onClick={
-                                headCell.order
-                                    ? createSortHandler(headCell.id)
-                                    : undefined
-                            }
-                        >
-                            {headCell.label}
-                            {orderBy === headCell.id ? (
-                                <Box sx={{ ...visuallyHidden }}>
-                                    {order === 'desc'
-                                        ? 'sorted descending'
-                                        : 'sorted ascending'}
-                                </Box>
-                            ) : null}
-                        </TableSortLabel>
-                    </StickyTableCell>
-                ))}
-            </TableRow>
-        </TableHead>
-    );
+  return (
+    <TableHead>
+      <TableRow style={{ background: '#00000000' }}>
+        {TABLE_HEAD(isMobile, timeFrame).map((headCell) => (
+          <StickyTableCell
+            key={headCell.id}
+            align={headCell.align}
+            sortDirection={orderBy === headCell.id ? order : false}
+            width={headCell.width}
+            sx={{
+              ...(headCell.no > 0 && {
+                pl: 0,
+                pr: 0
+              })
+            }}
+          >
+            <TableSortLabel
+              hideSortIcon
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'desc'}
+              onClick={headCell.order ? createSortHandler(headCell.id) : undefined}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box sx={{ ...visuallyHidden }}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </StickyTableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
 }
--- src/collection/CollectionList/Row.js
+++ src/collection/CollectionList/Row.js
@@ -1,14 +1,14 @@
 import {
-    styled,
-    IconButton,
-    Link,
-    Stack,
-    TableCell,
-    TableRow,
-    Tooltip,
-    Typography,
-    useTheme,
-    useMediaQuery
+  styled,
+  IconButton,
+  Link,
+  Stack,
+  TableCell,
+  TableRow,
+  Tooltip,
+  Typography,
+  useTheme,
+  useMediaQuery
 } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import VerifiedIcon from '@mui/icons-material/Verified';
@@ -16,9 +16,8 @@ import { formatMonthYearDate } from 'src/utils/formatTime';
 import { fNumber, fIntNumber, fVolume } from 'src/utils/formatNumber';
 import { Icon } from '@iconify/react';
 
-
 const IconCover = styled('div')(
-    ({ theme }) => `
+  ({ theme }) => `
         width: 30px;  // reduced from 40px
         height: 30px; // reduced from 40px
         box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
@@ -49,7 +48,7 @@ const IconCover = styled('div')(
 );
 
 const IconWrapper = styled('div')(
-    ({ theme }) => `
+  ({ theme }) => `
         box-sizing: border-box;
         display: inline-block;
         position: relative;
@@ -64,7 +63,7 @@ const IconWrapper = styled('div')(
 );
 
 const IconImage = styled('img')(
-    ({ theme }) => `
+  ({ theme }) => `
     position: absolute;
     inset: 0px;
     box-sizing: border-box;
@@ -83,176 +82,149 @@ const IconImage = styled('img')(
 );
 
 const ImageBackdrop = styled('span')(({ theme }) => ({
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundColor: theme.palette.common.black,
-    opacity: 0,
-    transition: theme.transitions.create('opacity')
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0,
+  transition: theme.transitions.create('opacity')
 }));
 
 export default function Row({ id, item, isMine, timeFrame }) {
-    const {
-        uuid,
-        account,
-        accountName,
-        name,
-        slug,
-        items,
-        type,
-        description,
-        logoImage,
-        featuredImage,
-        bannerImage,
-        costs,
-        extra,
-        minter,
-        verified,
-        created,
-        volume,
-        totalVolume,
-        floor,
-        owners,
-        vol24h,
-        totalVol24h
-    } = item;
+  const {
+    uuid,
+    account,
+    accountName,
+    name,
+    slug,
+    items,
+    type,
+    description,
+    logoImage,
+    featuredImage,
+    bannerImage,
+    costs,
+    extra,
+    minter,
+    verified,
+    created,
+    volume,
+    totalVolume,
+    floor,
+    owners,
+    vol24h,
+    totalVol24h
+  } = item;
 
-    const floorPrice = floor?.amount || 0;
-    const volumeValue = timeFrame === '24h' ? totalVol24h : totalVolume;
-    const volumeDisplay = fVolume(volumeValue || 0);
+  const floorPrice = floor?.amount || 0;
+  const volumeValue = timeFrame === '24h' ? totalVol24h : totalVolume;
+  const volumeDisplay = fVolume(volumeValue || 0);
 
-    const strDateTime = formatMonthYearDate(created);
-    const logoImageUrl = `https://s1.xrpnft.com/collection/${logoImage}`;
+  const strDateTime = formatMonthYearDate(created);
+  const logoImageUrl = `https://s1.xrpnft.com/collection/${logoImage}`;
 
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const handleRowClick = () => {
-        document.location = `/collection/${slug}`;
-    };
+  const handleRowClick = () => {
+    document.location = `/collection/${slug}`;
+  };
 
-    return (
-        <TableRow
-            hover
-            key={uuid}
-            onClick={handleRowClick}
-            style={{ cursor: 'pointer' }}
-        >
-            <TableCell align="left" sx={{ p: 0, border: 'none' }}>
-                <Stack
-                    direction="row"
-                    alignItems="center"
-                    spacing={2}
-                    sx={{ pt: 1, pb: 1 }}
+  return (
+    <TableRow hover key={uuid} onClick={handleRowClick} style={{ cursor: 'pointer' }}>
+      <TableCell align="left" sx={{ p: 0, border: 'none' }}>
+        <Stack direction="row" alignItems="center" spacing={2} sx={{ pt: 1, pb: 1 }}>
+          <Typography variant={isMobile ? 'caption' : 'body2'}>{id}</Typography>
+          <Link href={isMine ? `/collection/${slug}/edit` : `/collection/${slug}`} underline="none">
+            <IconCover>
+              <IconWrapper>
+                <IconImage src={logoImageUrl} />
+              </IconWrapper>
+
+              {isMine ? (
+                <IconButton
+                  className="MuiIconEditButton-root"
+                  aria-label="edit"
+                  sx={{
+                    position: 'absolute',
+                    left: '0vw',
+                    top: '0vh',
+                    opacity: 0,
+                    zIndex: 1,
+                    width: { xs: '18px', sm: '28px' }, // reduced sizes
+                    height: { xs: '18px', sm: '28px' } // reduced sizes
+                  }}
                 >
-                    <Typography
-                        variant={isMobile ? 'caption' : 'body2'} 
-                    >
-                        {id}
-                    </Typography>
-                    <Link
-                        href={
-                            isMine
-                                ? `/collection/${slug}/edit`
-                                : `/collection/${slug}`
-                        }
-                        underline="none"
-                    >
-                        <IconCover>
-                            <IconWrapper>
-                                <IconImage src={logoImageUrl} />
-                            </IconWrapper>
+                  <EditIcon />
+                </IconButton>
+              ) : (
+                <ImageBackdrop className="MuiImageBackdrop-root" />
+              )}
+            </IconCover>
+          </Link>
 
-                            {isMine ? (
-                                <IconButton
-                                    className="MuiIconEditButton-root"
-                                    aria-label="edit"
-                                    sx={{
-                                        position: 'absolute',
-                                        left: '0vw',
-                                        top: '0vh',
-                                        opacity: 0,
-                                        zIndex: 1,
-                                        width: { xs: '18px', sm: '28px' },  // reduced sizes
-                                        height: { xs: '18px', sm: '28px' }  // reduced sizes
-                                    }}
-                                >
-                                    <EditIcon />
-                                </IconButton>
-                            ) : (
-                                <ImageBackdrop className="MuiImageBackdrop-root" />
-                            )}
-                        </IconCover>
-                    </Link>
-
-                    <Link underline="none" href={`/collection/${slug}`}>
-                        <Stack spacing={0.4}>
-                            <Stack direction="row" spacing={0.5} sx={{ pt: 0 }}>
-                                <Typography
-                                    variant={isMobile ? 'caption' : 'token'} 
-                                    noWrap
-                                    color="primary"
-                                    sx={{
-                                        width: isMobile ? '80px' : undefined,
-                                        textOverflow: isMobile ? 'ellipsis' : 'none'
-                                    }}
-                                >
-                                    {name}
-                                </Typography>
-                            </Stack>
-                            <Typography
-                                variant={isMobile ? 'caption' : 'token'} 
-                                noWrap
-                            >
-                            </Typography>
-                        </Stack>
-                    </Link>
-                </Stack>
-            </TableCell>
-
-            <TableCell align="right" sx={{ pl: 0, pr: 0, border: 'none' }}>
-                <Typography variant={isMobile ? 'caption' : 'body2'} noWrap> 
-                <Typography>✕</Typography>{' '}
-                    {fNumber(floorPrice)}
+          <Link underline="none" href={`/collection/${slug}`}>
+            <Stack spacing={0.4}>
+              <Stack direction="row" spacing={0.5} sx={{ pt: 0 }}>
+                <Typography
+                  variant={isMobile ? 'caption' : 'token'}
+                  noWrap
+                  color="primary"
+                  sx={{
+                    width: isMobile ? '80px' : undefined,
+                    textOverflow: isMobile ? 'ellipsis' : 'none'
+                  }}
+                >
+                  {name}
                 </Typography>
-            </TableCell>
+              </Stack>
+              <Typography variant={isMobile ? 'caption' : 'token'} noWrap></Typography>
+            </Stack>
+          </Link>
+        </Stack>
+      </TableCell>
 
-            <TableCell align="right" sx={{ pl: 0, pr: 0, border: 'none' }}>
-                <Typography variant={isMobile ? 'caption' : 'body2'} noWrap> 
-                <Typography>✕</Typography>{' '}
-                    {volumeDisplay}
-                </Typography>
-            </TableCell>
+      <TableCell align="right" sx={{ pl: 0, pr: 0, border: 'none' }}>
+        <Typography variant={isMobile ? 'caption' : 'body2'} noWrap>
+          ✕ {fNumber(floorPrice)}
+        </Typography>
+      </TableCell>
 
-            <TableCell
-                align="right"
-                sx={{
-                    pl: 0,
-                    pr: 0,
-                    border: 'none',
-                    display: { xs: 'none', sm: 'table-cell' }
-                }}
-            >
-                <Typography variant={isMobile ? 'caption' : 'body2'} noWrap> 
-                    {fIntNumber(owners || 0)}
-                </Typography>
-            </TableCell>
+      <TableCell align="right" sx={{ pl: 0, pr: 0, border: 'none' }}>
+        <Typography variant={isMobile ? 'caption' : 'body2'} noWrap>
+          ✕ {volumeDisplay}
+        </Typography>
+      </TableCell>
 
-            <TableCell
-                align="right"
-                sx={{
-                    pl: 0,
-                    pr: 0,
-                    border: 'none',
-                    display: { xs: 'none', sm: 'table-cell' }
-                }}
-            >
-                <Typography variant={isMobile ? 'caption' : 'body2'} noWrap> 
-                    {fIntNumber(items)}
-                </Typography>
-            </TableCell>
-        </TableRow>
-    );
+      <TableCell
+        align="right"
+        sx={{
+          pl: 0,
+          pr: 0,
+          border: 'none',
+          display: { xs: 'none', sm: 'table-cell' }
+        }}
+      >
+        <Typography variant={isMobile ? 'caption' : 'body2'} noWrap>
+          {fIntNumber(owners || 0)}
+        </Typography>
+      </TableCell>
+
+      <TableCell
+        align="right"
+        sx={{
+          pl: 0,
+          pr: 0,
+          border: 'none',
+          display: { xs: 'none', sm: 'table-cell' }
+        }}
+      >
+        <Typography variant={isMobile ? 'caption' : 'body2'} noWrap>
+          {fIntNumber(items)}
+        </Typography>
+      </TableCell>
+    </TableRow>
+  );
 }
