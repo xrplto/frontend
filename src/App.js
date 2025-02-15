0643d3b0c938f612441d9321c3c7deca6d04408c
xrplto <hello@xrpl.to>
2025-02-15 13:46:55 -0500

    removed random portfolio code here

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Portfolio from './portfolio';
 
 const theme = createTheme({
   palette: {
     mode: 'dark', // or 'light'
     primary: {
-      main: '#90caf9',
+      main: '#90caf9'
     },
     secondary: {
-      main: '#f48fb1',
+      main: '#f48fb1'
     },
     divider: '#707070', // example color, you can customize this
     text: {
       primary: '#ffffff',
-      secondary: '#aaaaaa',
-    },
-  },
+      secondary: '#aaaaaa'
+    }
+  }
 });
 
 const root = createRoot(document.getElementById('root'));
@@ -27,7 +26,6 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Portfolio account="example" limit={10} collection="exampleCollection" type="exampleType" />
     </ThemeProvider>
   </React.StrictMode>
 );
