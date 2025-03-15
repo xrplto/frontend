1104a4ed2888ee1dd135bd1dcf6bae82b311ee40
xrplto <hello@xrpl.to>
2025-03-15 05:29:52 -0400

    fixed environment variables

--- next.config.js
+++ next.config.js
@@ -4,44 +4,44 @@
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
 //     enabled: process.env.ANALYZE === 'true',
 // })
-  
+
 // module.exports = withBundleAnalyzer({ })
 
-const isProd = process.env.RUN_ENV === 'development'
+const isProd = process.env.RUN_ENV === 'production';
 module.exports = {
-    poweredByHeader: false,
-    assetPrefix: isProd ? 'https://xrpl.to' : '',
-	env: {
-		API_URL: process.env.API_URL,
-		MAINTENANCE: process.env.MAINTENANCE,
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/most-viewed-tokens',
-				destination: '/rankings/most-viewed-tokens',
-			},
-			{
-				source: '/best-tokens',
-				destination: '/rankings/best-tokens',
-			},
-			{
-				source: '/trending-tokens',
-				destination: '/rankings/trending-tokens',
-			},
-			{
-				source: '/gainers-losers',
-				destination: '/rankings/gainers-losers',
-			},
-			{
-				source: '/new',
-				destination: '/rankings/new',
-			},
-		]
-	},
-	// Add performance optimizations
-	reactStrictMode: true,
-	compress: true,
-	optimizeFonts: true,
-	swcMinify: true,
-}
+  poweredByHeader: false,
+  assetPrefix: isProd ? 'https://xrpl.to' : '',
+  env: {
+    API_URL: process.env.API_URL,
+    MAINTENANCE: process.env.MAINTENANCE
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/most-viewed-tokens',
+        destination: '/rankings/most-viewed-tokens'
+      },
+      {
+        source: '/best-tokens',
+        destination: '/rankings/best-tokens'
+      },
+      {
+        source: '/trending-tokens',
+        destination: '/rankings/trending-tokens'
+      },
+      {
+        source: '/gainers-losers',
+        destination: '/rankings/gainers-losers'
+      },
+      {
+        source: '/new',
+        destination: '/rankings/new'
+      }
+    ];
+  },
+  // Add performance optimizations
+  reactStrictMode: true,
+  compress: true,
+  optimizeFonts: true,
+  swcMinify: true
+};
