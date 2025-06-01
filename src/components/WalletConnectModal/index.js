ee7e064dc5199a9f7f6c253bf1d3aa7d1d42d9a2
xrplto <hello@xrpl.to>
2025-06-01 10:04:59 -0400

    ui update

--- src/components/WalletConnectModal/index.js
+++ src/components/WalletConnectModal/index.js
@@ -7,24 +7,24 @@ import { Divider, IconButton, Stack, Typography } from '@mui/material';
 import styled from '@emotion/styled';
 import ClearIcon from '@mui/icons-material/Clear';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import DownloadIcon from '@mui/icons-material/Download';
 import { enqueueSnackbar } from 'notistack';
 
-import { isInstalled, getPublicKey, signMessage } from "@gemwallet/api";
-import sdk from "@crossmarkio/sdk";
+import { isInstalled, getPublicKey, signMessage } from '@gemwallet/api';
+import sdk from '@crossmarkio/sdk';
 
 import LoginDialog from '../LoginDialog';
 import { AppContext } from 'src/AppContext';
 import axios from 'axios';
 
 const WalletItem = styled(Stack)(({ theme }) => ({
-  padding: "15px",
-  cursor: "pointer",
-  transition: "all 0.2s",
-  "&:hover": {
+  padding: '15px',
+  cursor: 'pointer',
+  transition: 'all 0.2s',
+  '&:hover': {
     backgroundColor: theme.general.backgroundAlt
   }
 }));
-
 
 const WalletConnectModal = () => {
   const BASE_URL = process.env.API_URL;
@@ -42,7 +42,6 @@ const WalletConnectModal = () => {
     doLogIn
   } = useContext(AppContext);
 
-
   const handleClose = () => {
     setOpenWalletModal(false);
   };
@@ -54,35 +53,40 @@ const WalletConnectModal = () => {
           // console.log(`${response.result?.address} - ${response.result?.publicKey}`);
           const pubkey = response.result?.publicKey;
           //fetch nonce from /api/gem/nonce?pubkey=pubkey
-          await axios.get(
-            `${BASE_URL}/account/auth/gem/nonce?pubkey=${pubkey}&address=${response.result?.address}`
-          )
+          await axios
+            .get(
+              `${BASE_URL}/account/auth/gem/nonce?pubkey=${pubkey}&address=${response.result?.address}`
+            )
             .then((res) => {
               const nonceToken = res.data.token;
               const opts = {
                 headers: {
-                  "Content-Type": "application/json",
-                  Authorization: `Bearer ${nonceToken}`,
-                },
+                  'Content-Type': 'application/json',
+                  Authorization: `Bearer ${nonceToken}`
+                }
               };
               signMessage(nonceToken).then(async (response) => {
                 const signedMessage = response.result?.signedMessage;
                 if (signedMessage !== undefined) {
-                  await axios.post(`${BASE_URL}/account/auth/gem/check-sign?signature=${signedMessage}`, {}, opts)
+                  await axios
+                    .post(
+                      `${BASE_URL}/account/auth/gem/check-sign?signature=${signedMessage}`,
+                      {},
+                      opts
+                    )
                     .then((res) => {
                       const { profile } = res.data;
-                      doLogIn({ ...profile, wallet_type: "gem" });
+                      doLogIn({ ...profile, wallet_type: 'gem' });
                       setOpenWalletModal(false);
                     });
                 }
               });
             });
         });
+      } else {
+        enqueueSnackbar('GemWallet is not installed', { variant: 'error' });
       }
-      else {
-        enqueueSnackbar("GemWallet is not installed", { variant: "error" });
-      }
-    })
+    });
   };
 
   const handleConnectCrossmark = async () => {
@@ -93,32 +97,33 @@ const WalletConnectModal = () => {
       // }
       // const { isCrossmark } = window.xrpl;
       // if (isCrossmark) {
-        const hashR = await axios.get(`${BASE_URL}/account/auth/crossmark/hash`);
-        const hashJson = hashR.data;
-        const hash = hashJson.hash;
-        const id = await sdk.methods.signInAndWait(hash);
-        const address = id.response.data.address;
-        const pubkey = id.response.data.publicKey;
-        const signature = id.response.data.signature;
-        await axios.post(
+      const hashR = await axios.get(`${BASE_URL}/account/auth/crossmark/hash`);
+      const hashJson = hashR.data;
+      const hash = hashJson.hash;
+      const id = await sdk.methods.signInAndWait(hash);
+      const address = id.response.data.address;
+      const pubkey = id.response.data.publicKey;
+      const signature = id.response.data.signature;
+      await axios
+        .post(
           `${BASE_URL}/account/auth/crossmark/check-sign?signature=${signature}`,
           {
             pubkey: pubkey,
-            address: address,
+            address: address
           },
           {
             headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${hash}`,
-            },
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${hash}`
+            }
           }
-        ).then((res) => {
+        )
+        .then((res) => {
           const { profile } = res.data;
-          doLogIn({ ...profile, wallet_type: "crossmark" });
+          doLogIn({ ...profile, wallet_type: 'crossmark' });
           setOpenWalletModal(false);
-        }).catch(err => {
-
-        });
+        })
+        .catch((err) => {});
       // }
     } catch (err) {
       console.log(err);
@@ -133,8 +138,8 @@ const WalletConnectModal = () => {
       aria-describedby="scroll-dialog-description"
       PaperProps={{
         sx: {
-          width: "100%",
-          maxWidth: "450px",
+          width: '100%',
+          maxWidth: '450px'
         }
       }}
       sx={{
@@ -142,57 +147,132 @@ const WalletConnectModal = () => {
       }}
     >
       <DialogTitle>
-        <Stack direction="row" justifyContent={openLogin ? "space-between" : "end"}>
-          {
-            openLogin &&
+        <Stack direction="row" justifyContent={openLogin ? 'space-between' : 'end'}>
+          {openLogin && (
             <IconButton onClick={onLogoutXumm}>
               <ArrowBackIcon />
             </IconButton>
-          }
+          )}
           <IconButton onClick={handleClose}>
             <ClearIcon />
           </IconButton>
         </Stack>
       </DialogTitle>
-      <DialogContent sx={{ paddingBottom: "60px" }}>
-        {
-          openLogin ?
-            <LoginDialog
-              open={openLogin}
-              handleClose={handleLoginClose}
-              qrUrl={qrUrl}
-              nextUrl={nextUrl}
-            />
-            :
-            <>
-              <Typography variant="modal" mb={2}>Login</Typography>
-              <Stack
-                spacing={0.5}
+      <DialogContent sx={{ paddingBottom: '60px' }}>
+        {openLogin ? (
+          <LoginDialog
+            open={openLogin}
+            handleClose={handleLoginClose}
+            qrUrl={qrUrl}
+            nextUrl={nextUrl}
+          />
+        ) : (
+          <>
+            <Typography variant="modal" mb={2}>
+              Login
+            </Typography>
+            <Stack spacing={1.5}>
+              <WalletItem direction="row" spacing={2} alignItems="center" onClick={handleLogin}>
+                <img src="/icons/xaman.png" alt="Xaman Wallet" width="40" height="40" />
+                <Stack sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6" component="div">
+                    Xaman
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Mobile Wallet
+                  </Typography>
+                </Stack>
+                <Typography
+                  component="a"
+                  href="https://xaman.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  sx={{
+                    marginLeft: 'auto',
+                    color: 'text.secondary',
+                    '&:hover': { color: 'primary.main' }
+                  }}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <IconButton size="small" aria-label="Download Xaman Wallet">
+                    <DownloadIcon fontSize="inherit" />
+                  </IconButton>
+                </Typography>
+              </WalletItem>
+              <Divider />
+
+              <WalletItem
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                onClick={handleConnectGem}
               >
-                <WalletItem direction="row" spacing={2} alignItems="center" onClick={handleLogin}>
-                  <img src="/icons/xaman.png" />
-                  <Typography variant="h3">Xaman</Typography>
-                </WalletItem>
-                <Divider />
+                <img src="/icons/gem.svg" alt="GemWallet" width="40" height="40" />
+                <Stack sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6" component="div">
+                    GemWallet
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Browser Wallet
+                  </Typography>
+                </Stack>
+                <Typography
+                  component="a"
+                  href="https://gemwallet.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  sx={{
+                    marginLeft: 'auto',
+                    color: 'text.secondary',
+                    '&:hover': { color: 'primary.main' }
+                  }}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <IconButton size="small" aria-label="Download GemWallet">
+                    <DownloadIcon fontSize="inherit" />
+                  </IconButton>
+                </Typography>
+              </WalletItem>
+              <Divider />
 
-                <WalletItem direction="row" spacing={2} alignItems="center" onClick={handleConnectGem}>
-                  <img src="/icons/gem.svg" />
-                  <Typography variant="h3">GemWallet</Typography>
-                </WalletItem>
-                <Divider />
-
-                <WalletItem direction="row" spacing={2} alignItems="center" onClick={handleConnectCrossmark}>
-                  <img src="/icons/crossmark.png" />
-                  <Typography variant="h3">CrossMark</Typography>
-                </WalletItem>
-              </Stack>
-            </>
-        }
-
-
+              <WalletItem
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                onClick={handleConnectCrossmark}
+              >
+                <img src="/icons/crossmark.png" alt="CrossMark Wallet" width="40" height="40" />
+                <Stack sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6" component="div">
+                    CrossMark
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Browser Wallet
+                  </Typography>
+                </Stack>
+                <Typography
+                  component="a"
+                  href="https://crossmark.io/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  sx={{
+                    marginLeft: 'auto',
+                    color: 'text.secondary',
+                    '&:hover': { color: 'primary.main' }
+                  }}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <IconButton size="small" aria-label="Download CrossMark Wallet">
+                    <DownloadIcon fontSize="inherit" />
+                  </IconButton>
+                </Typography>
+              </WalletItem>
+            </Stack>
+          </>
+        )}
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
 export default WalletConnectModal;
