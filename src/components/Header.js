8ded7cd6a52cb127282e9d36fe9fa3e248df930e
xrplto <hello@xrpl.to>
2025-07-01 04:33:06 -0400

    ui update

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import { useTranslation } from 'react-i18next';
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext, useEffect, forwardRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import sdk from "@crossmarkio/sdk";
 import { AppContext } from 'src/AppContext';
@@ -34,8 +34,6 @@ import WalletConnectModal from './WalletConnectModal';
 import { selectProcess, updateProcess } from 'src/redux/transactionSlice';
 
 // Iconify
-import { Icon } from '@iconify/react';
-import chartLineUp from '@iconify/icons-ph/chart-line-up';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import FiberNewIcon from '@mui/icons-material/FiberNew';
@@ -63,6 +61,45 @@ const XPMarketIcon = (props) => {
     </SvgIcon>
   );
 };
+
+const LedgerMemeIcon = forwardRef((props, ref) => {
+  // Filter out any non-DOM props that might cause warnings
+  const { width, darkMode, ...otherProps } = props;
+
+  return (
+    <SvgIcon {...otherProps} ref={ref} viewBox="0 0 26 26">
+      <g transform="scale(0.55)">
+        <rect fill="#cfff04" width="36" height="36" rx="8" ry="8" x="0" y="0"></rect>
+        <g>
+          <g>
+            <path
+              fill="#262626"
+              d="M25.74,9.68c0.64,0,1.24-0.26,1.69-0.72l2.69-2.76h-1.64l-1.87,1.92c-0.23,0.24-0.55,0.37-0.88,0.37s-0.64-0.13-0.88-0.37l-1.87-1.92h-1.64l2.69,2.76c0.45,0.46,1.05,0.72,1.69,0.72Z"
+            ></path>
+            <path
+              fill="#262626"
+              d="M27.43,10.62c-0.45-0.46-1.05-0.72-1.69-0.72s-1.24,0.26-1.69,0.72l-2.71,2.78h1.64l1.89-1.93c0.23-0.24,0.55-0.37,0.88-0.37s0.64,0.13,0.88,0.37l1.89,1.93h1.64l-2.71-2.78Z"
+            ></path>
+            <path
+              fill="#262626"
+              d="M10.22,9.68c0.64,0,1.24-0.26,1.69-0.72l2.69-2.76h-1.64l-1.87,1.92c-0.23,0.24-0.55,0.37-0.88,0.37s-0.64-0.13-0.88-0.37l-1.87-1.92h-1.64l2.69,2.76c0.45,0.46,1.05,0.72,1.69,0.72Z"
+            ></path>
+            <path
+              fill="#262626"
+              d="M10.22,9.90c-0.64,0-1.24,0.26-1.69,0.72l-2.71,2.78h1.64l1.89-1.93c0.23-0.24,0.55-0.37,0.88-0.37s0.64,0.13,0.88,0.37l1.89,1.93h1.64l-2.71-2.78c-0.45-0.46-1.05-0.72-1.69-0.72Z"
+            ></path>
+          </g>
+          <path
+            fill="#262626"
+            d="M5.81,17.4c0,6.73,5.45,12.18,12.18,12.18s12.18-5.45,12.18-12.18H5.81Z"
+          ></path>
+        </g>
+      </g>
+    </SvgIcon>
+  );
+});
+
+LedgerMemeIcon.displayName = 'LedgerMemeIcon';
 
 const HeaderWrapper = styled(Box)(
   ({ theme }) => `
@@ -474,7 +511,7 @@ export default function Header(props) {
                 <StyledMenuItem
                   darkMode={darkMode}
                   theme={theme}
-                  onClick={() => handleTokenOptionSelect('/view/xrpfun')}
+                  onClick={() => handleTokenOptionSelect('/view/ledgermeme')}
                   sx={{
                     display: 'flex',
                     alignItems: 'center',
@@ -482,16 +519,8 @@ export default function Header(props) {
                   }}
                 >
                   <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                    <Icon
-                      icon={chartLineUp}
-                      style={{
-                        fontSize: '16px',
-                        color: '#B72136',
-                        backgroundColor: '#fff',
-                        borderRadius: '2px'
-                      }}
-                    />
-                    <span>xrp.fun</span>
+                    <LedgerMemeIcon sx={{ fontSize: '16px', color: '#cfff04' }} />
+                    <span>LedgerMeme</span>
                   </Box>
                 </StyledMenuItem>
 
