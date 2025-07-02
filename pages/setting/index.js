d172ae814ebe9d9c25a3f89d2b8e6c8c2716c5a0
xrplto <hello@xrpl.to>
2025-07-02 15:41:26 -0400

    fix

--- pages/setting/index.js
+++ pages/setting/index.js
@@ -1,65 +1,56 @@
 // Material
-import {
-    Box,
-    Container,
-    styled,
-    Toolbar
-} from '@mui/material';
+import { Box, Container, styled, Toolbar } from '@mui/material';
 
 // Components
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-import EditProfile from 'src/account/setting';
 import ScrollToTop from 'src/components/ScrollToTop';
 
 const OverviewWrapper = styled(Box)(
-    ({ theme }) => `
+  ({ theme }) => `
         // overflow: hidden;
         flex: 1;
 `
 );
 
-export default function Overview({data}) {
+export default function Overview({ data }) {
+  return (
+    <OverviewWrapper>
+      <Toolbar id="back-to-top-anchor" />
 
-    return (
-        <OverviewWrapper>
-            <Toolbar id="back-to-top-anchor" />
+      <Header />
 
-            <Header />
+      <Container maxWidth="sm"></Container>
 
-            <Container maxWidth="sm">
-                <EditProfile />
-            </Container>
+      <ScrollToTop />
 
-            <ScrollToTop />
-
-            <Footer />
-
-        </OverviewWrapper>
-    );
+      <Footer />
+    </OverviewWrapper>
+  );
 }
 
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
 export async function getStaticProps() {
-    let ret = {};
+  let ret = {};
 
-    const ogp = {};
+  const ogp = {};
 
-    ogp.canonical = 'https://xrpl.to/setting';
-    ogp.title = 'Setting';
-    ogp.url = 'https://xrpl.to/setting';
-    ogp.imgUrl = 'https://xrpl.to/static/ogp.webp';
-    ogp.desc = 'Customize your profile on our settings page: Edit your profile details, upload a logo and banner image, and update your display name and description with ease.';
+  ogp.canonical = 'https://xrpl.to/setting';
+  ogp.title = 'Setting';
+  ogp.url = 'https://xrpl.to/setting';
+  ogp.imgUrl = 'https://xrpl.to/static/ogp.webp';
+  ogp.desc =
+    'Customize your profile on our settings page: Edit your profile details, upload a logo and banner image, and update your display name and description with ease.';
 
-    ret = {ogp};
+  ret = { ogp };
 
-    return {
-        props: ret, // will be passed to the page component as props
-        // Next.js will attempt to re-generate the page:
-        // - When a request comes in
-        // - At most once every 10 seconds
-        // revalidate: 10, // In seconds
-    }
+  return {
+    props: ret // will be passed to the page component as props
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    // revalidate: 10, // In seconds
+  };
 }
