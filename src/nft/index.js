b9b23912479dc30d956da04a02305c677f780883
xrplto <hello@xrpl.to>
2025-08-12 05:19:27 -0400

    fixes

--- src/nft/index.js
+++ src/nft/index.js
@@ -10,7 +10,6 @@ import { NFToken } from "src/utils/constants";
 
 // Components
 import NFTDetails from './NFTDetails';
-import NFTDetailsMobile from './NFTDetailsMobile';
 import NFTActions from './NFTActions';
 import NFTActionsBulk from './NFTActionsBulk';
 
@@ -24,20 +23,17 @@ export default function Detail({nft}) {
     } = nft;
 
     return (
-        isMobile && status !== NFToken.SELL_WITH_MINT_BULK ?
-            <NFTDetailsMobile nft={nft} />
-            :
-            <Grid container spacing={3} justifyContent='center'>
-                <Grid item xs={12} md={5.16}>{/* trying to fit 480px image, was 5/7 */}
-                    <NFTDetails nft={nft} />
-                </Grid>
-                <Grid item xs={12} md={6.84}>
-                    {status === NFToken.SELL_WITH_MINT_BULK ?
-                        <NFTActionsBulk nft={nft} />
-                    :
-                        <NFTActions nft={nft} />
-                    }
-                </Grid>
+        <Grid container spacing={3} justifyContent='center'>
+            <Grid item xs={12} md={5.16}>{/* trying to fit 480px image, was 5/7 */}
+                <NFTDetails nft={nft} />
             </Grid>
+            <Grid item xs={12} md={6.84}>
+                {status === NFToken.SELL_WITH_MINT_BULK ?
+                    <NFTActionsBulk nft={nft} />
+                :
+                    <NFTActions nft={nft} />
+                }
+            </Grid>
+        </Grid>
     );
 }
