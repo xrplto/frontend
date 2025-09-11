bed0a86afef1406b9e831c0c445c2cf74ba31a1a
xrplto <hello@xrpl.to>
2025-09-11 11:15:02 -0400

    fixes

--- next.config.js
+++ next.config.js
@@ -25,10 +25,10 @@ const config = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
-    qualities: [75, 90, 100],
-    minimumCacheTTL: 3600,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    quality: 80,
+    minimumCacheTTL: 86400,
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
@@ -222,6 +222,14 @@ const config = {
     esmExternals: true,
     webpackMemoryOptimizations: true,
     optimizePackageImports: ['@mui/material', '@mui/icons-material', '@mui/lab'],
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production'
@@ -263,10 +271,17 @@ const config = {
               },
               // Large vendor libraries
               vendor: {
-                test: /[\\/]node_modules[\\/](react|react-dom|axios|lodash)[\\/]/,
+                test: /[\\/]node_modules[\\/](react|react-dom|axios|lodash|@reduxjs)[\\/]/,
                 name: 'vendor',
                 priority: 10,
                 chunks: 'all',
+              },
+              // Swiper and slider components
+              sliders: {
+                test: /[\\/]node_modules[\\/](swiper|keen-slider)[\\/]/,
+                name: 'sliders',
+                priority: 25,
+                chunks: 'async',
               },
               // Iconify icons
               iconify: {
